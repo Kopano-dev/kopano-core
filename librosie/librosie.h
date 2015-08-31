a848dc094bbec0cddff8dbb12df112f1e8cafb29
Jan Engelhardt <jengelh@inai.de>
2015-08-31 08:07:52 +0200

    librosie: improvements all over the place
    * use C++-style headers (cstdio) and casts (reinterpret_cast<>)
    * add missing includes
    * add "static" keyword to all non-exported symbols
    * use ARRAY_SIZE and drop the redundant NULL sentinel
    * initialize tidy buffers properly with libtidy API
    * shorten x::iterator with a typedef

--- librosie/librosie.h
+++ librosie/librosie.h
@@ -1,5 +1,10 @@
+#ifndef _LIBROSIE_H
+#define _LIBROSIE_H 1
+
 #include <kopano/zcdefs.h>
 #include <string>
 #include <vector>
 
 extern _kc_export bool CleanHtml(const std::string & in, std::string *const out, std::vector<std::string> *const errors);
+
+#endif /* LIBROSIE_H */
