9cf605e1ddabc9c8bacc3c52a9ada96e9b41af1d
Mark Dufour <m.dufour@kopano.com>
2016-07-15 15:20:49 +0200

    python-mapi: register MAPIUninitialize to be called at exit
    Even though leaking once at initialization time is not so bad, this
    avoids many valgrind errors.
    
    We call gc.collect() first, because we probably want uninitialization
    to be really the last step before exiting. if we do not do this, we
    will get segfaults after running certain tests.
    
    References: KC-95

--- swig/python/MAPI/__init__.py
+++ swig/python/MAPI/__init__.py
@@ -1,8 +1,15 @@
+import atexit
+import gc
 import sys
+
 from MAPICore import *
 
+def mapi_uninitialize():
+    gc.collect() # do this early, so we don't touch objects after uninitialization (avoiding a crash for certain tests)
+    MAPIUninitialize()
 
 MAPIInitialize_Multithreaded()
+atexit.register(mapi_uninitialize)
 
 unicode = False
 
