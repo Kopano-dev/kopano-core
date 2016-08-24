4c18ec696d69538fd44ce6ef5787bf02bbda293f
Jan Engelhardt <development@kopano.io>
2016-08-24 09:27:41 +0200

    tools: add searchfolder_create.py
    Debugging utility for KC-140.

---
+++ tools/searchfolder_create.py
@@ -0,0 +1,9 @@
+# Create a lot of dummy SFs
+import kopano
+s = kopano.Server(auth_user='bar', auth_pass='xbar')
+u = s.user('bar')
+for x in range(0, 10000):
+	findroot = u.root.folder('FINDER_ROOT') # search folders are stored here as regular MAPI folders, but with the data coming from special DB tables
+	print 'searchfolder count:', findroot.subfolder_count # count search folders
+	sf = u.create_searchfolder() # create search folder in 'findroot'
+	sf.search_start(u.inbox, 'blah')
