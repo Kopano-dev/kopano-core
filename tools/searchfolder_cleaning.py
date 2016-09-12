f4657caff4fe2f93e33193fdd0b21dd2cb9ed28d
Joost Hopmans <j.hopmans@kopano.com>
2016-09-12 12:09:04 +0200

    tools: add utility for cleaning searchfolders
    Without deleting permanent searchfolders, clear SFs.

---
+++ tools/searchfolder_cleaning.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import datetime
+
+import kopano
+
+for u in kopano.users():
+    print('User: %s' % u.name)
+    findroot = u.findroot
+    if not findroot:
+        print('No findroot for user "%s"' % u.name)
+        continue  
+  
+    # Saved searchfolder list
+    saved_sf = list(u.store.searches())
+
+    # If the search folder is a permanent search folder, we keep it.
+    # If it's a normal search folder that's older than 7 days, remove it for performance improvement.
+    for sf in findroot.folders():
+        if sf not in saved_sf:
+            if sf.created < datetime.datetime.now() - datetime.timedelta(days=7):
+                print('Removing searchfolder "%s"' % sf.name)
+                findroot.delete(sf)
