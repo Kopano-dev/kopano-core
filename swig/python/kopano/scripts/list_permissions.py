fd1de9262a9fada2a73fb580fa8b76fe21c45a64
Mark Dufour <m.dufour@kopano.com>
2016-11-15 10:17:27 +0100

    pyko: add list_permissions script

---
+++ swig/python/kopano/scripts/list_permissions.py
@@ -0,0 +1,15 @@
+import kopano
+
+#user1 = kopano.user('user1')
+#user2 = kopano.user('user2')
+#user1.inbox.permission(user2, create=True).rights = ['read_items', 'delete_own']
+
+for store in kopano.stores():
+    for permission in store.permissions():
+        print 'store', store, permission.member, permission.rights
+    for folder in store.folders():
+        for permission in folder.permissions():
+            print 'folder', store, folder, permission.member, permission.rights
+
+
+
