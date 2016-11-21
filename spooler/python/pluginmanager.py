f2f4bd492234a36495946d5c29729b129a223b19
Bo Simonsen <b.simonsen@kopano.com>
2016-11-21 12:32:04 +0100

    swig: pep8 fixes on pluginmanager.py

--- spooler/python/pluginmanager.py
+++ spooler/python/pluginmanager.py
@@ -8,6 +8,7 @@ import functools
 
 from plugintemplates import *
 
+
 class PluginManager(object):
     def __init__(self, plugindir, logger):
         self.logger = logger
@@ -20,14 +21,17 @@ class PluginManager(object):
             raise Exception('Invalid plugins directory')
 
         directory = os.path.abspath(self.plugindir)
-        if os.path.isdir(directory) == False:
-            self.logger.logWarn("! Plugins directory '%s' doesn't exists. Plugins not loaded." % directory)
+        if not os.path.isdir(directory):
+            self.logger.logWarn(
+                "! Plugins directory '%s' doesn't exists."
+                "Plugins not loaded." % directory
+            )
             return 0
 
         plugins = glob.glob(directory + "/*.py")
 
         self.logger.logInfo("** Checking plugins in %s" % directory)
-        if not directory in sys.path:
+        if directory not in sys.path:
             sys.path.insert(0, directory)
 
         for p in plugins:
@@ -35,7 +39,8 @@ class PluginManager(object):
                 name = p.split("/")[-1]
                 name = name.split(".py")[0]
 
-                if name.startswith('_'): continue
+                if name.startswith('_'):
+                    continue
 
                 self.logger.logDebug("** Inspecting file '%s.py'" % name)
 
@@ -43,15 +48,18 @@ class PluginManager(object):
 
                 plugin = __import__(name)
 
-                for k,v in inspect.getmembers(plugin):
-                    if not inspect.isclass(v): continue
-                    if k.startswith('_'): continue
+                for k, v in inspect.getmembers(plugin):
+                    if not inspect.isclass(v):
+                        continue
+                    if k.startswith('_'):
+                        continue
 
                     if not issubclass(v, baseclass) or v is baseclass:
                         continue
 
                     instance = v(self.logger)
-                    self.logger.logInfo("** Adding plugin '%s'" % k) # fixme, use a plugin meta name ?
+                    # fixme, use a plugin meta name ?
+                    self.logger.logInfo("** Adding plugin '%s'" % k)
                     self.plugins.append((k, instance))
 
             except Exception as e:
@@ -68,43 +76,66 @@ class PluginManager(object):
         # get the priority of the plugin function
         for (name, i) in self.plugins:
 
-           if hasattr(i, 'prio'+functionname):
-               prio = getattr(i, 'prio'+functionname)
-           else:
-               prio = 9999
+            if hasattr(i, 'prio' + functionname):
+                prio = getattr(i, 'prio' + functionname)
+            else:
+                prio = 9999
 
-           plugins.append((functionname, prio, name, i))
+            plugins.append((functionname, prio, name, i))
 
         # sort the plugins on function name and prio
-        sortedlist = sorted(plugins, key = lambda a: (a[0], a[1]))
+        sortedlist = sorted(plugins, key=lambda a: (a[0], a[1]))
 
         retval = MP_CONTINUE,
         for (fname, prio, cname, i) in sortedlist:
-            if (fname != functionname): continue
+            if (fname != functionname):
+                continue
             try:
-                self.logger.logInfo("** Plugin '%s.%s' priority (%d)" % (cname, fname, prio))
+                self.logger.logInfo("** Plugin '%s.%s' priority (%d)" % (
+                    cname, fname, prio
+                ))
 
                 retval = getattr(i, fname)(*args)
 
-                self.logger.logDebug("** Plugin '%s.%s' return result %s"  % (cname, fname, repr(retval) ))
+                self.logger.logDebug("** Plugin '%s.%s' return result %s" % (
+                    cname, fname, repr(retval)
+                ))
 
                 if (type(retval) == int or type(retval) == long):
-                    self.logger.logDebug("!- Plugin '%s.%s' convert return value type '%s' to a 'tuple'"  % (cname, fname, type(retval)))
+                    self.logger.logDebug(
+                        "!- Plugin '%s.%s' convert return value type "
+                        "'%s' to a 'tuple'" % (cname, fname, type(retval))
+                    )
                     retval = retval,
                 if (type(retval) != tuple):
-                    self.logger.logWarn("!- Plugin '%s.%s' returned a wrong return value type '%s' except 'tuple'. Fallback on defaults" % (cname, fname, type(retval)))
+                    self.logger.logWarn(
+                        "!- Plugin '%s.%s' returned a wrong"
+                        "return value type '%s' except 'tuple'. "
+                        "Fallback on defaults" % (cname, fname, type(retval))
+                    )
                     retval = MP_CONTINUE,
                 elif (type(retval[0]) != int and type(retval[0]) != long):
-                    self.logger.logWarn("!- Plugin '%s.%s' returned a wrong return value type '%s' expect 'int' or 'long' type. Fallback on defaults" % (cname, fname, type(retval[0])))
+                    self.logger.logWarn(
+                        "!- Plugin '%s.%s' returned a wrong return value "
+                        "type '%s' expect 'int' or 'long' type. Fallback "
+                        "on defaults" % (cname, fname, type(retval[0]))
+                    )
                     retval = MP_CONTINUE,
 
-                self.logger.logInfo("** Plugin '%s.%s' returncode %d" % (cname, fname, retval[0]))
+                self.logger.logInfo(
+                    "** Plugin '%s.%s' returncode %d" % (
+                        cname, fname, retval[0]
+                    )
+                )
             except NotImplementedError as e:
-                #Most likely the function is not implemented
+                # Most likely the function is not implemented
                 self.logger.logDebug("!---------- %s" % e)
             except Exception as e:
                 self.logger.logError("!-- error: %s " % e)
-                self.logger.logError("!- Plugin '%s.%s' call ignored please check the plugin" % (cname, fname))
+                self.logger.logError(
+                    "!- Plugin '%s.%s' call ignored please "
+                    "check the plugin" % (cname, fname)
+                )
                 retval = MP_CONTINUE,
 
             if (retval[0] == MP_EXIT):
@@ -112,4 +143,3 @@ class PluginManager(object):
 
         self.logger.logInfo("* %s processing done" % functionname)
         return retval
-
