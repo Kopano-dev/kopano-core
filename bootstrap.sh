2521206c594e5b0836813f3034f6e64f59c0e345
Jan Engelhardt <development@kopano.io>
2016-12-03 00:48:22 +0100

    build: have bootstrap.sh check for pkg-config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+if ! which pkg-config >/dev/null 2>/dev/null; then
+	# If you do not have this, useless autoconf error messages
+	# can result: possibly undefined macro AC_CHECK_LIB
+	echo "pkg-config seems to be missing";
+	exit 1
+fi
 exec autoreconf -fiv
