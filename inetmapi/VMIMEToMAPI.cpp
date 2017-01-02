e3995a979a7693d2e4f51d0c65512a48a2b31ce2
Jan Engelhardt <development@kopano.io>
2017-01-02 19:32:17 +0100

    treewide: remove dependency on boost string operations
    References: KC-451

--- inetmapi/VMIMEToMAPI.cpp
+++ inetmapi/VMIMEToMAPI.cpp
@@ -30,11 +30,6 @@
 #include <cctype>
 #include <cstdlib>
 #include <cstring>
-
-// string.hpp doesn't always include all subincludes
-#include <boost/algorithm/string/replace.hpp>
-
-// vmime
 #include <vmime/vmime.hpp>
 #include <vmime/platforms/posix/posixHandler.hpp>
 #include <vmime/contentTypeField.hpp>
@@ -3245,7 +3240,7 @@ std::string VMIMEToMAPI::mailboxToEnvelope(vmime::shared_ptr<vmime::mailbox> mbo
 	buffer = "\"" + mbox->getEmail().toString() + "\"";
 	pos = buffer.find("@");
 	if (pos != string::npos)
-		boost::algorithm::replace_first(buffer, "@", "\" \"");
+		buffer.replace(pos, 1, "\" \"");
 	lMBox.push_back(std::move(buffer));
 	if (pos == string::npos)
 		lMBox.push_back("NIL");	// domain was missing
