97b45530e1d183e02755e04dd7760297a2d69b88
Mark Dufour <m.dufour@kopano.com>
2017-01-28 23:07:57 +0100

    pyko: allow looping over (rule) actions
    allow looping over (rule) actions, by adding an Action
    class and making Rule.actions yield instances of this class.
    
    KC-512

--- swig/python/kopano/kopano/rule.py
+++ swig/python/kopano/kopano/rule.py
@@ -5,7 +5,9 @@ Copyright 2005 - 2016 Zarafa and its licensors (see LICENSE file for details)
 Copyright 2016 - Kopano and its licensors (see LICENSE file for details)
 """
 
-from MAPI.Tags import PR_RULE_NAME, PR_RULE_STATE, ST_ENABLED
+from MAPI.Tags import (
+    PR_RULE_NAME, PR_RULE_STATE, PR_RULE_PROVIDER, PR_RULE_ACTIONS, ST_ENABLED
+)
 
 from .compat import repr as _repr, fake_unicode as _unicode
 
@@ -14,13 +16,17 @@ from .restriction import Restriction
 class Rule(object):
     def __init__(self, mapirow):
         self.mapirow = mapirow
-        name, state = mapirow[PR_RULE_NAME], mapirow[PR_RULE_STATE]
-        self.name = _unicode(name)
-        self.active = bool(state & ST_ENABLED)
+        self.name = _unicode(mapirow[PR_RULE_NAME])
+        self.provider = _unicode(mapirow[PR_RULE_PROVIDER])
+        self.active = bool(mapirow[PR_RULE_STATE] & ST_ENABLED)
 
     @property
     def restriction(self):
         return Restriction(mapiobj=self.mapirow[PR_RULE_CONDITION])
+
+    def actions(self):
+        for action in self.mapirow[PR_RULE_ACTIONS].lpAction:
+            yield Action(action)
 
     def __unicode__(self):
         return u"Rule('%s')" % self.name
@@ -28,3 +34,27 @@ class Rule(object):
     def __repr__(self):
         return _repr(self)
 
+class Action(object):
+    acttype_str = {
+        1: 'move',
+        2: 'copy',
+        3: 'reply',
+        4: 'oof_reply',
+        5: 'defer',
+        6: 'bounce',
+        7: 'forward',
+        8: 'delegate',
+        9: 'tag',
+        10: 'delete',
+        11: 'mark_read',
+    }
+
+    def __init__(self, mapiobj):
+        self.mapiobj = mapiobj
+        self.operator = self.acttype_str[mapiobj.acttype]
+
+    def __unicode__(self):
+        return u"Action('%s')" % self.operator
+
+    def __repr__(self):
+        return _repr(self)
