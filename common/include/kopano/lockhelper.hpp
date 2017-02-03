e7a7c86f195977a4801a20e8c312dc24770b4ec9
Jan Engelhardt <jengelh@inai.de>
2017-02-03 15:31:41 +0100

    common: remove old pthread_rwlock wrapper
    References: KC-462

--- common/include/kopano/lockhelper.hpp
+++ common/include/kopano/lockhelper.hpp
@@ -52,27 +52,6 @@ template<class _Mutex> class shared_lock {
 	bool locked = false;
 };
 
-template<int (*fnlock)(pthread_rwlock_t *)> class scoped_rwlock _kc_final {
-	public:
-	scoped_rwlock(pthread_rwlock_t &rwlock) : m_rwlock(rwlock)
-	{
-		fnlock(&m_rwlock);
-	}
-
-	~scoped_rwlock(void)
-	{
-		pthread_rwlock_unlock(&m_rwlock);
-	}
-
-	private:
-	scoped_rwlock(const scoped_rwlock &) = delete;
-	scoped_rwlock &operator=(const scoped_rwlock &) = delete;
-
-	pthread_rwlock_t &m_rwlock;
-};
-
-typedef scoped_rwlock<pthread_rwlock_wrlock> scoped_exclusive_rwlock;
-typedef scoped_rwlock<pthread_rwlock_rdlock> scoped_shared_rwlock;
 typedef std::lock_guard<std::mutex> scoped_lock;
 typedef std::lock_guard<std::recursive_mutex> scoped_rlock;
 typedef std::unique_lock<std::mutex> ulock_normal;
