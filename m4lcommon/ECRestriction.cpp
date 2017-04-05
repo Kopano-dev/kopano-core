38f9f7f36ca584c0e55dec6dcb18adb9b8e0c07f
Jan Engelhardt <jengelh@inai.de>
2017-04-05 23:16:53 +0200

    m4lcommon: fix freeing invalid pointer in ECRestriction
    When lpBase is set, lpPropDst is managed by that base, so the
    CopyProp function must not attempt to free it.

--- m4lcommon/ECRestriction.cpp
+++ m4lcommon/ECRestriction.cpp
@@ -109,7 +109,7 @@ HRESULT ECRestriction::CopyProp(SPropValue *lpPropSrc, void *lpBase,
 	lpPropDst = NULL;
 
 exit:
-	if (lpBase != NULL)
+	if (lpBase == nullptr)
 		MAPIFreeBuffer(lpPropDst);
 
 	return hr;
@@ -153,7 +153,7 @@ HRESULT ECRestriction::CopyPropArray(ULONG cValues, SPropValue *lpPropSrc,
 	lpPropDst = NULL;
 
 exit:
-	if (lpBase != NULL)
+	if (lpBase == nullptr)
 		MAPIFreeBuffer(lpPropDst);
 
 	return hr;
