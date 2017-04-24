fa0c862452c0f79c1ed5a7f1bb03f897af824868
Mark Dufour <m.dufour@kopano.com>
2017-04-24 11:13:25 +0200

    pyko: change default config loglevel to 'warning'
    Change default config loglevel to 'warning', as this is the
    general default for KC (and we actually suggest this is the
    case in our config files).
    
    References: KC-639

--- swig/python/kopano/kopano/config.py
+++ swig/python/kopano/kopano/config.py
@@ -151,7 +151,7 @@ Example::
 
 CONFIG = {
     'log_method': Config.string(options=['file', 'syslog'], default='file'),
-    'log_level': Config.string(options=[str(i) for i in range(7)] + ['info', 'debug', 'warning', 'error', 'critical'], default='info'),
+    'log_level': Config.string(options=[str(i) for i in range(7)] + ['info', 'debug', 'warning', 'error', 'critical'], default='warning'),
     'log_file': Config.string(default=None),
     'log_timestamp': Config.integer(options=[0, 1], default=1),
     'pid_file': Config.string(default=None),
