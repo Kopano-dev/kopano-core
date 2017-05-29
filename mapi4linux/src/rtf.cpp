dd93c71b19899054631e79bcac4e159835402cd5
Jan Engelhardt <jengelh@inai.de>
2017-05-29 22:59:45 +0200

    ECtools: add rtfcomp test utility

--- mapi4linux/src/rtf.cpp
+++ mapi4linux/src/rtf.cpp
@@ -19,6 +19,8 @@
 #include <cstdlib>
 #include "rtf.h"
 
+namespace KC {
+
 static const unsigned int crctable[256] = {
   0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
   0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
@@ -410,3 +412,5 @@ exit:
 	free(lpDest);
 	return ulRetVal;
 }
+
+} /* namespace KC */
--- mapi4linux/src/rtf.h
+++ mapi4linux/src/rtf.h
@@ -18,8 +18,14 @@
 #ifndef RTF_H
 #define RTF_H
 
-extern unsigned int rtf_get_uncompressed_length(const char *data, unsigned int size);
-extern unsigned int rtf_decompress(char *dst, const char *src, unsigned int src_size);
-extern unsigned int rtf_compress(char **dst, unsigned int *dst_size, const char *src, unsigned int src_size);
+#include <kopano/zcdefs.h>
+
+namespace KC {
+
+extern _kc_export unsigned int rtf_get_uncompressed_length(const char *data, unsigned int size);
+extern _kc_export unsigned int rtf_decompress(char *dst, const char *src, unsigned int src_size);
+extern _kc_export unsigned int rtf_compress(char **dst, unsigned int *dst_size, const char *src, unsigned int src_size);
+
+}
 
 #endif // RTF_H
