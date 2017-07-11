608330f65bb38d29bfe290d6431b986b48dbb316
Jan Engelhardt <jengelh@inai.de>
2017-07-11 17:56:40 +0200

    provider: don't use memset for non-unions
    (Just for the looks.)

--- provider/common/soapctor.cpp
+++ provider/common/soapctor.cpp
@@ -4,7 +4,6 @@
 #include "soapStub.h"
 #define C(X) X::X(void) { static_assert(std::is_standard_layout<X>::value, ""); memset(this, 0, sizeof(*this)); }
 C(_act)
-C(propTagArray)
 C(propValData)
 
 mv_binary::mv_binary() : __ptr(), __size() {}
@@ -15,7 +14,7 @@ mv_i8::mv_i8() : __ptr(), __size() {}
 mv_long::mv_long() : __ptr(), __size() {}
 mv_r4::mv_r4() : __ptr(), __size() {}
 mv_string8::mv_string8() : __ptr(), __size() {}
-
+propTagArray::propTagArray() : __ptr(), __size() {}
 propTagArray::propTagArray(unsigned int *a, int b) :
 	__ptr(a), __size(b)
 {}
