3832819f46cfa4672e779080867896324cefce2a
Jelle van der Waa <j.vanderwaa@kopano.com>
2017-09-19 11:47:33 +0200

    tools: Make print statements Python 3 compatible
    Make searchfolder_delete Python 3 compatible by changing the print
    statements.

--- tools/searchfolder_delete.py
+++ tools/searchfolder_delete.py
@@ -3,17 +3,17 @@ import kopano
 from MAPI.Tags import PR_CREATION_TIME
 
 for u in kopano.users():
-    print 'user', u
+    print('user {}'.format(u.name))
     try:
         findroot = u.root.folder('FINDER_ROOT')
     except:
-        print 'ERROR getting findroot, skipping user'
+        print('ERROR getting findroot, skipping user')
         continue
-    print findroot, findroot.subfolder_count
+    print('{} {}'.format(findroot.name, findroot.subfolder_count))
     for sf in findroot.folders():
-        print sf, sf.entryid, sf.hierarchyid
+        print('{} {} {}'.format(sf.name, sf.entryid, sf.hierarchyid))
         creation_time = sf.prop(PR_CREATION_TIME).value
-        print 'created at', creation_time
+        print( 'created at ' + creation_time)
         if creation_time < datetime.datetime.now()-datetime.timedelta(days=30):
-            print 'DELETING!'
+            print('DELETING!')
             findroot.delete(sf)
