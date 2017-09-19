c34f2012f2ab9cd408b507098dcf82554a5e900c
Jelle van der Waa <j.vanderwaa@kopano.com>
2017-09-19 11:48:35 +0200

    tools: Use folder.created for deleting searchfolders
    Use the newly created property folder.created to remove the last
    dependency on Python-MAPI

--- tools/searchfolder_delete.py
+++ tools/searchfolder_delete.py
@@ -1,6 +1,5 @@
 import datetime
 import kopano
-from MAPI.Tags import PR_CREATION_TIME
 
 for u in kopano.users():
     print('user {}'.format(u.name))
@@ -12,8 +11,7 @@ for u in kopano.users():
     print('{} {}'.format(findroot.name, findroot.subfolder_count))
     for sf in findroot.folders():
         print('{} {} {}'.format(sf.name, sf.entryid, sf.hierarchyid))
-        creation_time = sf.prop(PR_CREATION_TIME).value
-        print( 'created at ' + creation_time)
-        if creation_time < datetime.datetime.now()-datetime.timedelta(days=30):
+        print( 'created at ' + sf.created)
+        if sf.created < datetime.datetime.now()-datetime.timedelta(days=30):
             print('DELETING!')
             findroot.delete(sf)
