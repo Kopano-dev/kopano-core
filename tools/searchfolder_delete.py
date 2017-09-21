678f507ffd8c8bcd5800e002593b97afde904354
Mark Dufour <m.dufour@kopano.com>
2017-09-21 14:07:26 +0200

    searchfolder_delete.py: simplify python3 compat
    Replace python3 compatibility work-around, by just importing
    'from __future__ import print_function'.

--- tools/searchfolder_delete.py
+++ tools/searchfolder_delete.py
@@ -1,17 +1,19 @@
+from __future__ import print_function
+
 import datetime
 import kopano
 
 for u in kopano.users():
-    print('user {}'.format(u.name))
+    print('user', u.name)
     try:
         findroot = u.root.folder('FINDER_ROOT')
     except:
         print('ERROR getting findroot, skipping user')
         continue
-    print('{} {}'.format(findroot.name, findroot.subfolder_count))
+    print(findroot.name, findroot.subfolder_count)
     for sf in findroot.folders():
-        print('{} {} {}'.format(sf.name, sf.entryid, sf.hierarchyid))
-        print( 'created at ' + sf.created)
+        print(sf.name, sf.entryid, sf.hierarchyid)
+        print('created at ' + sf.created)
         if sf.created < datetime.datetime.now()-datetime.timedelta(days=30):
             print('DELETING!')
             findroot.delete(sf)
