5e657a5c9686c8c473f723422f098512790a2d4f
Jan Engelhardt <jengelh@inai.de>
2017-10-27 02:47:27 +0200

    ldapplugin: fix out-of-bounds in b64_encode
    When the input length is a multiple of 3, b64_encode does a round too
    much. Replace all invocations by base64_encode.
    
    This is currently not exploitable, since MD5 is 16 bytes, SMD5 is 20
    bytes, SHA is 20 bytes, and SSHA… is, as it turns out, completely
    misconstructed, emitting only 20 instead of 24 bytes.

--- provider/plugins/ldappasswords.cpp
+++ provider/plugins/ldappasswords.cpp
@@ -33,51 +33,6 @@ using namespace std;
 
 namespace KC {
 
-static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-/** 
- * Encode string in base-64
- *
- * @param[out] out the buffer to which the result will be written.
- * @param[in] in the input string, not necessarily 0-terminated.
- * @param[in] len the number of bytes from \c in to encode.
- * 
- * The buffer \c out must be large enough to hold the result, which is
- * \c len / 3 * 4 + 4 (approximately).
- */
-static void b64_encode(char *out, const unsigned char *in, unsigned int len) {
-	unsigned int i, j;
-	unsigned char bytes[3];
-
-	for (i = j = 0; i < len / 3 * 3 + 3; i += 3) {
-		bytes[0] = in[i];
-		out[j++] = b64chars[(bytes[0] >> 2) & 0x3f];
-
-		if (i + 1 >= len)
-			bytes[1] = 0;
-		else
-			bytes[1] = in[i + 1];
-
-		out[j++] = b64chars[((bytes[0] << 4) & 0x30) | ((bytes[1] >> 4) & 0x0f)];
-
-		if (i + 2 >= len)
-			bytes[2] = 0;
-		else
-			bytes[2] = in[i + 2];
-
-		if (i + 1 < len)
-			out[j++] = b64chars[((bytes[1] << 2) & 0x3c) | ((bytes[2] >> 6) & 0x03)];
-		else
-			out[j++] = '=';
-		if (i + 2 < len)
-			out[j++] = b64chars[bytes[2] & 0x3f];
-		else
-			out[j++] = '=';
-	}
-
-	out[j++] = 0;
-}
-
 static char *password_encrypt_crypt(const char *data, unsigned int len) {
 	char salt[3];
 	rand_get(salt, 2);
@@ -111,29 +66,18 @@ static int password_check_crypt(const char *data, unsigned int len, const char *
 static char *password_encrypt_md5(const char *data, unsigned int len) {
 	unsigned char md5_out[MD5_DIGEST_LENGTH];
 	const int base64_len = MD5_DIGEST_LENGTH * 4 / 3 + 4;
-	char b64_out[MD5_DIGEST_LENGTH * 4 / 3 + 4];
 	char *res;
 
 	MD5((unsigned char *) data, len, md5_out);
-	b64_encode(b64_out, md5_out, MD5_DIGEST_LENGTH);
-
 	res = new char[base64_len + 12];
-	snprintf(res, base64_len + 11, "{MD5}%s", b64_out);
-
+	snprintf(res, base64_len + 11, "{MD5}%s", base64_encode(md5_out, sizeof(md5_out)).c_str());
 	return res;
 }
 
 static int password_check_md5(const char *data, unsigned int len, const char *crypted) {
 	unsigned char md5_out[MD5_DIGEST_LENGTH];
-	char b64_out[MD5_DIGEST_LENGTH * 4 / 3 + 4];
-
 	MD5((unsigned char *) data, len, md5_out);
-	b64_encode(b64_out, md5_out, MD5_DIGEST_LENGTH);
-
-	if (!strcmp(b64_out, crypted))
-		return 0;
-	else
-		return 1;
+	return strcmp(crypted, base64_encode(md5_out, sizeof(md5_out)).c_str());
 }
 
 // md5sum + salt at the end. md5sum length == 16, salt length == 4
@@ -142,7 +86,6 @@ static char *password_encrypt_smd5(const char *data, unsigned int len) {
 	unsigned char md5_out[MD5_DIGEST_LENGTH + 4];
 	unsigned char *salt = md5_out + MD5_DIGEST_LENGTH; // salt is at the end of the digest
 	constexpr size_t base64_len = sizeof(md5_out) * 4 / 3 + 4;
-	char b64_out[base64_len];
 	char *res;
 
 	rand_get(reinterpret_cast<char *>(salt), 4);
@@ -150,12 +93,8 @@ static char *password_encrypt_smd5(const char *data, unsigned int len) {
 	MD5_Update(&ctx, data, len);
 	MD5_Update(&ctx, salt, 4);
 	MD5_Final(md5_out, &ctx);	// writes upto the salt
-
-	b64_encode(b64_out, md5_out, MD5_DIGEST_LENGTH + 4);
-
 	res = new char[base64_len + 12];
-	snprintf(res, base64_len + 11, "{SMD5}%s", b64_out);
-
+	snprintf(res, base64_len + 11, "{SMD5}%s", base64_encode(md5_out, sizeof(md5_out)).c_str());
 	return res;
 }
 
@@ -163,7 +102,6 @@ static int password_check_smd5(const char *data, unsigned int len, const char *c
 	std::string digest;
 	std::string salt;
 	unsigned char md5_out[MD5_DIGEST_LENGTH];
-	char b64_out[MD5_DIGEST_LENGTH * 4 / 3 + 4];
 	MD5_CTX ctx;
 
 	digest = base64_decode(crypted);
@@ -175,15 +113,12 @@ static int password_check_smd5(const char *data, unsigned int len, const char *c
 	MD5_Update(&ctx, data, len);
 	MD5_Update(&ctx, salt.c_str(), salt.length());
 	MD5_Final(md5_out, &ctx);
-
-	b64_encode(b64_out, md5_out, MD5_DIGEST_LENGTH);
-	return strcmp(b64_out, crypted);
+	return strcmp(crypted, base64_encode(md5_out, sizeof(md5_out)).c_str());
 }
 
 static char *password_encrypt_ssha(const char *data, unsigned int len, bool bSalted) {
 	unsigned char SHA_out[SHA_DIGEST_LENGTH];
 	const int base64_len = SHA_DIGEST_LENGTH * 4 / 3 + 4;
-	char b64_out[SHA_DIGEST_LENGTH * 4 / 3 + 4];
 	char *res;
 	unsigned char salt[4];
 	std::string pwd;
@@ -195,11 +130,9 @@ static char *password_encrypt_ssha(const char *data, unsigned int len, bool bSal
 	}
 
 	SHA1((const unsigned char*)pwd.c_str(), pwd.length(), SHA_out);
-	b64_encode(b64_out, SHA_out, SHA_DIGEST_LENGTH);
-
 	res = new char[base64_len + 12];
-	snprintf(res, base64_len + 11, "{%s}%s", bSalted ? "SSHA" : "SHA", b64_out);
-
+	snprintf(res, base64_len + 11, "{%s}%s", bSalted ? "SSHA" : "SHA",
+	         base64_encode(SHA_out, SHA_DIGEST_LENGTH).c_str());
 	return res;
 }
 
