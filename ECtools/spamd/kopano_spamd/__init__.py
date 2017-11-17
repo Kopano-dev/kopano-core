f0ffefe62d3d359e894910715d17f3d2387688c2
Bo Simonsen <b.simonsen@kopano.com>
2017-11-17 15:42:14 +0100

    spamd: indent reduction and simplification
    Simplified control structures like try .. except and
    reduced the indent in general.
    
    References: KC-666

--- ECtools/spamd/kopano_spamd/__init__.py
+++ ECtools/spamd/kopano_spamd/__init__.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-# coding=utf-8
-"""
-ICS driven spam learning daemon for Kopano / SpamAssasin
-See included readme.md for more information.
-"""
-
 from .version import __version__
 
 import os
@@ -14,9 +8,11 @@ import kopano
 from kopano import Config, log_exc
 from contextlib import closing
 
+"""
+kopano-spamd - ICS driven spam learning daemon for Kopano / SpamAssasin
+"""
+
 CONFIG = {
-    'run_as_user': Config.string(default="kopano"),
-    'run_as_group': Config.string(default="kopano"),
     'spam_dir': Config.string(default="/var/lib/kopano/spamd/spam"),
     'sa_group': Config.string(default="amavis")
 }
@@ -32,11 +28,7 @@ class Service(kopano.Service):
                 try:
                     state = server.sync(catcher, state)
                 except Exception as e:
-                    if e.hr == MAPI_E_NETWORK_ERROR:
-                        self.log.info('Trying to reconnect to Server in %s seconds' % 5)
-                    else:
-                        self.log.info('Error: [%s]' % e)
-                    time.sleep(5)
+                    self.log.info('Error: [%s]' % e)
                 time.sleep(1)
 
 
@@ -47,27 +39,26 @@ class Checker(object):
         self.sagroup = service.config['sa_group']
 
     def update(self, item, flags):
-        if item.message_class == 'IPM.Note':
-            if item.folder == item.store.user.junk and not item.header('x-spam-flag') == 'YES':
-                self.learn(item)
+        learn = item.message_class == 'IPM.Note' and \
+            item.folder == item.store.junk and \
+            item.header('x-spam-flag') != 'YES'
+
+        if learn:
+            self.learn(item)
 
     def learn(self, item):
-        with log_exc(self.log):
-            try:
-                entryid = item.entryid
-                spameml = item.eml()
-                havespam = True
-            except Exception as e:
-                self.log.info('Failed to extract eml of email: [%s] [%s]' % (e, item.entryid))
-            if havespam:
-                try:
-                    emlfilename = os.path.join(self.spamdir, entryid)
-                    with closing(open(emlfilename, "w")) as fh:
-                        fh.write(spameml)
-                    shutil.chown(emlfilename, group=self.sagroup)
-                except Exception as e:
-                    self.log.info('could not write to spam dir: [%s] [%s]' % (e, item.entryid))
-
+        try:
+            entryid = item.entryid
+            spameml = item.eml()
+            emlfilename = os.path.join(self.spamdir, entryid)
+            with closing(open(emlfilename, "wb")) as fh:
+                fh.write(spameml)
+            shutil.chown(emlfilename, group=self.sagroup)
+        except Exception as e:
+            self.log.info(
+                'Exception happend during learning: [%s] [%s]' %
+                (e, item.entryid)
+            )
 
 def main():
     parser = kopano.parser('ckpsF')  # select common cmd-line options
