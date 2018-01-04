fbb53ed19402d6dd8019107908f95ef42cc26d78
Jan Engelhardt <jengelh@inai.de>
2018-01-04 13:46:19 +0100

    php7-ext: re-work g++-5 workarounds for missing imports
    phpconfig.h changed a little between Ubuntu 16.04 and Ubuntu 16.04
    Update (it lost a "#if ... defined(zend_isfinite)". Rather than
    trying to hack phpconfig.h, replay the parts from
    /usr/include/c++/6/math.h that contribute to isfinite issues under
    g++-5.

--- php7-ext/ECImportHierarchyChangesProxy.cpp
+++ php7-ext/ECImportHierarchyChangesProxy.cpp
@@ -22,6 +22,10 @@
 #include <cstdio>
 #include <ctime>
 #include <cmath>
+#if __GNUC_PREREQ(5, 0) && !__GNUC_PREREQ(6, 0)
+using std::isfinite;
+using std::isnan;
+#endif
 
 extern "C" {
 	// Remove these defines to remove warnings
@@ -30,12 +34,6 @@ extern "C" {
 	#undef PACKAGE_NAME
 	#undef PACKAGE_STRING
 	#undef PACKAGE_BUGREPORT
-
-	#if !__GNUC_PREREQ(6,0)
-	#define zend_isfinite(a) std::isfinite(a)
-	#define zend_isnan(a) std::isnan(a)
-	#endif
-	
 	#include "php.h"
    	#include "php_globals.h"
 	#include "ext/standard/info.h"
