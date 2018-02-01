643a0344a13e993b8cfcbe3237eb380d51c23a92
Jan Engelhardt <jengelh@inai.de>
2018-02-01 12:16:43 +0100

    provider: restore setting explicit value for user::ulIsNonActive
    valgrind trips when trying to serialize user::ulIsNonActive:
    
    	Use of uninitialised value of size 8
    	at 0xA89616B: _itoa_word (_itoa.c:179)
    	by 0xA89A869: vfprintf (vfprintf.c:1636)
    	by 0xA8C1E58: vsnprintf (vsnprintf.c:114)
    	by 0xA8A1291: snprintf (snprintf.c:33)
    	by 0x5663E05: soap_unsignedLong2s (in /home/srepmub/kc/.libs/libkcserver.so.0.0.0)
    	by 0x5663A30: soap_outunsignedInt (in /home/srepmub/kc/.libs/libkcserver.so.0.0.0)
    	by 0x5977BEB: soap_out_user(soap*, char const*, int, user const*, char const*) (soapC.cpp:40495)
    	by 0x5977D99: soap_out_userArray(soap*, char const*, int, userArray const*, char const*) (soapC.cpp:40336)
    	by 0x5977E4D: soap_out_userListResponse(soap*, char const*, int, userListResponse const*, char const*) (soapC.cpp:40212)
    	by 0x5983F67: soap_put_userListResponse(soap*, userListResponse const*, char const*, char const*) (soapC.cpp:40293)
    	by 0x13B30A: soap_serve_ns__getUserList(soap*) (soapServer.cpp:2978)
    	by 0x12FD1E: ECWorkerThread::Work(void*) (ECThreadManager.cpp:221)
    	by 0x9D9F493: start_thread (pthread_create.c:333)
    	by 0xA93AAFE: clone (clone.S:97)
    	Uninitialised value was created by a heap allocation
    	at 0x4C2BBCD: malloc (vg_replace_malloc.c:299)
    	by 0x5653F53: soap_malloc (in /home/srepmub/kc/.libs/libkcserver.so.0.0.0)
    	by 0x5609214: s_alloc<user> (SOAPAlloc.h:39)
    	by 0x5609214: operator() (cmd.cpp:5163)
    	by 0x5609214: ns__getUserList(soap*, unsigned long, unsigned int, xsd__base64Binary, userListResponse*) (cmd.cpp:5188)
    
    Fixes: kopanocore-8.4.90-1159-g406c1ec2
    References: KC-1008

--- provider/include/proto.h
+++ provider/include/proto.h
@@ -598,7 +598,7 @@ struct user {
 	char		*lpszMailAddress;
 	char		*lpszFullName;
 	char		*lpszServername;
-	unsigned int 	ulIsNonActive; // unused
+	unsigned int 	ulIsNonActive; /* was used for 6.40 clients */
 	unsigned int 	ulIsAdmin;
 	unsigned int	ulIsABHidden;
 	unsigned int	ulCapacity;
