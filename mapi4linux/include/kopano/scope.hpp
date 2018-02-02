88d718866a9a4eda105f3863523d283cacb6b943
Jan Engelhardt <jengelh@inai.de>
2018-02-02 10:24:36 +0100

    hl: add noexcept specifier for scope_success
    CID-182634 (#1 of 1): Uncaught exception (UNCAUGHT_EXCEPT)
    exn_spec_violation: An exception of type KC::KMAPIError is thrown but
    the throw list throw() doesn't allow it to be thrown.
    
    References: KC-1010

--- mapi4linux/include/kopano/scope.hpp
+++ mapi4linux/include/kopano/scope.hpp
@@ -8,14 +8,15 @@ namespace KC {
 
 /* P0052r5 (C++2020) */
 template<typename F> class scope_success {
-	public:
-	explicit scope_success(F &&f) : m_func(std::move(f)), m_eod(true) {}
-	scope_success(scope_success &&o) : m_func(std::move(o.m_func)), m_eod(o.m_eod) {}
-	~scope_success() { if (m_eod && !std::uncaught_exception()) m_func(); }
-	void operator=(scope_success &&) = delete;
 	private:
 	F m_func;
 	bool m_eod = false;
+
+	public:
+	explicit scope_success(F &&f) : m_func(std::move(f)), m_eod(true) {}
+	scope_success(scope_success &&o) : m_func(std::move(o.m_func)), m_eod(o.m_eod) {}
+	~scope_success() noexcept(noexcept(m_func())) { if (m_eod && !std::uncaught_exception()) m_func(); }
+	void operator=(scope_success &&) = delete;
 };
 
 template<typename F> scope_success<F> make_scope_success(F &&f)
