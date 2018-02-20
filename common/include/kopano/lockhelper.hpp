2700af3b0d69677ca1cc94e94f172492e0726bb4
Jan Engelhardt <jengelh@inai.de>
2018-02-20 13:36:37 +0100

    common: use std shared mutexes and locks when available

--- common/include/kopano/lockhelper.hpp
+++ common/include/kopano/lockhelper.hpp
@@ -3,14 +3,19 @@
 
 #include <kopano/zcdefs.h>
 #include <mutex>
-#include <new>
+#if __cplusplus >= 201400L
+#	include <shared_mutex>
+#endif
 #include <cassert>
 #include <pthread.h>
 
 namespace KC {
 
-/* std::shared_mutex only available from C++17 onwards */
-
+#if __cplusplus >= 201700L
+using shared_mutex = std::shared_mutex;
+#elif __cplusplus >= 201400L
+using shared_mutex = std::shared_timed_mutex;
+#else
 class shared_mutex {
 	public:
 	~shared_mutex(void) { pthread_rwlock_destroy(&mtx); }
@@ -22,7 +27,11 @@ class shared_mutex {
 	private:
 	pthread_rwlock_t mtx = PTHREAD_RWLOCK_INITIALIZER;
 };
+#endif
 
+#if __cplusplus >= 201400L
+template<class Mutex> using shared_lock = std::shared_lock<Mutex>;
+#else
 template<class _Mutex> class shared_lock {
 	public:
 	shared_lock(_Mutex &m) : mtx(m), locked(true)
@@ -50,6 +59,7 @@ template<class _Mutex> class shared_lock {
 	_Mutex &mtx;
 	bool locked = false;
 };
+#endif
 
 typedef std::lock_guard<std::mutex> scoped_lock;
 typedef std::lock_guard<std::recursive_mutex> scoped_rlock;
