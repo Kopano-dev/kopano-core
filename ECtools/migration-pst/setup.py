0a81d594a505ea1810a768b1d6bff1472d963059
Simon Eisenmann <simon@longsleep.org>
2018-03-02 14:48:06 +0100

    migration-pst: Build with setuptools
    This change makes kopano_migration_pst a distributable Python module
    including the correct version number in its meta data.

--- ECtools/migration-pst/setup.py
+++ ECtools/migration-pst/setup.py
@@ -1,6 +1,41 @@
-from distutils.core import setup, Extension
+import io
+import os.path
+import re
+import subprocess
+
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+try:
+    with io.open(os.path.join(here, '..', '..', '.version'), encoding='utf-8') as version_file:
+        metadata = {
+            'version': version_file.readline().strip()
+        }
+except FileNotFoundError:
+    try:
+        v = subprocess.check_output(['../../tools/describe_version']).decode('utf-8').strip()[11:]
+    except FileNotFoundError:
+        v = 'dev'
+
+    metadata = {
+        'version': v,
+        'withoutVersionFile': True
+    }
+
 
 setup(name='kopano-migration-pst',
-      version='0.1',
-      packages=['kopano_migration_pst'],
+      version=metadata['version'],
+      url='https://kopano.io',
+      author='Kopano',
+      author_email='development@kopano.io',
+      keywords=['kopano'],
+      license='AGPL',
+      packages=find_packages()
 )
