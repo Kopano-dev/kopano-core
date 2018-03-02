dd25078696e07d8b7948af334f4510d1cfdbdb2e
Simon Eisenmann <simon@longsleep.org>
2018-03-02 14:30:50 +0100

    cli: Build with setuptools
    This change makes kopano_cli a redistributable Python module including
    the correct version number in its meta data.

--- ECtools/cli/setup.py
+++ ECtools/cli/setup.py
@@ -1,6 +1,55 @@
-from distutils.core import setup, Extension
+import io
+import os.path
+import re
+import subprocess
+
+from distutils.command.build_py import build_py
+from setuptools import setup, find_packages
+
+
+version_base = 'kopano_cli'
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+try:
+    with io.open(os.path.join(here, version_base, 'version.py'), encoding='utf-8') as version_file:
+        metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", version_file.read()))
+except FileNotFoundError:
+    try:
+        v = subprocess.check_output(['../../tools/describe_version']).decode('utf-8').strip()[11:]
+    except FileNotFoundError:
+        v = 'dev'
+
+    metadata = {
+        'version': v,
+        'withoutVersionFile': True
+    }
+
+
+class my_build_py(build_py, object):
+    def run(self):
+        super(my_build_py, self).run()
+
+        if metadata.get('withoutVersionFile', False):
+            with io.open(os.path.join(self.build_lib, version_base, 'version.py'), mode='w') as version_file:
+                version_file.write('__version__ = "%s"\n' % metadata['version'])
+
 
 setup(name='kopano-cli',
-      version='0.1',
-      packages=['kopano_cli'],
+      version=metadata['version'],
+      url='https://kopano.io',
+      description='Admin utils for Kopano Core',
+      long_description='Common command-line utilities for use with Kopano Core',
+      author='Kopano',
+      author_email='development@kopano.io',
+      keywords=['kopano'],
+      license='AGPL',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      cmdclass={'build_py': my_build_py}
 )
