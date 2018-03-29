Kopano-dev/kopano-core

commit b3bf36fbb00b7c5dfb8ddd7909d32c817ee3f8e0

provider: speed up getIDsFromNames
getIDsFromNames for N properties resulted in N single database
queries. This has now been improved so N properties results in
1 query.

provider/libserver/cmd.cpp
@@ -4405,16 +4405,12 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 		goto exit;
 	}
 
-	er = lpDatabase->Begin();
-	if(er != erSuccess)
-		goto exit;
-
 	lpsResponse->lpsPropTags.__ptr = s_alloc<unsigned int>(soap, lpsNamedProps->__size);
 	lpsResponse->lpsPropTags.__size = 0;
 
-	// One query per named property (too slow ?) FIXME could be faster if brought down to less SQL queries
+	strQuery = "SELECT id, nameid, namestring, guid FROM names WHERE ";
 	for (gsoap_size_t i = 0; i < lpsNamedProps->__size; ++i) {
-		strQuery = "SELECT id FROM names WHERE ";
+		strQuery += "(";
 
 		// ID, then add ID where clause
 		if(lpsNamedProps->__ptr[i].lpId != NULL) {
@@ -4427,6 +4423,7 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 
 			strQuery += "namestring='" + strEscapedString + "' ";
 		}
+		/* else { Handle this, it will break the SQL query, iff guid is present } */
 
 		// Add a GUID specifier if there
 		if(lpsNamedProps->__ptr[i].lpguid != NULL) {
@@ -4434,73 +4431,87 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 
 			strQuery += "AND guid=" + strEscapedGUID;
 		}
-		strQuery += " LIMIT 1";
 
-		// Run the query
-		er = lpDatabase->DoSelect(strQuery, &lpDBResult);
-		if(er != erSuccess)
-			goto exit;
+		strQuery += ")";
+		if (i != lpsNamedProps->__size - 1)
+			strQuery += " OR ";
+	}
 
-		if (lpDBResult.get_num_rows() == 0) {
-			// No rows found, so the named property has not been registered yet
+	er = lpDatabase->DoSelect(strQuery, &lpDBResult);
+	if(er != erSuccess)
+		goto exit;
 
-			if(ulFlags & MAPI_CREATE) {
-				// Create requested ? then add a new named property
+	for (gsoap_size_t i = 0; i < lpsNamedProps->__size; ++i)
+		lpsResponse->lpsPropTags.__ptr[i] = 0;
 
-				// GUID must be specified with create
-				if(lpsNamedProps->__ptr[i].lpguid == NULL) {
-					er = KCERR_NO_ACCESS;
-					goto exit;
-				}
+	for (size_t i = 0; i < lpDBResult.get_num_rows(); ++i) {
+		lpDBRow = lpDBResult.fetch_row();
+		if (lpDBRow == nullptr)
+			continue;
+		for (gsoap_size_t i = 0; i < lpsNamedProps->__size; ++i) {
+			std::string nameid, namestring;
 
-				strQuery = "INSERT INTO names (nameid, namestring, guid) VALUES(";
+			if (lpsNamedProps->__ptr[i].lpId != nullptr)
+				nameid = stringify(*lpsNamedProps->__ptr[i].lpId);
+			else if (lpsNamedProps->__ptr[i].lpString != nullptr)
+				namestring = lpDatabase->Escape(lpsNamedProps->__ptr[i].lpString);
 
-				if (lpsNamedProps->__ptr[i].lpId)
-					strQuery += stringify(*lpsNamedProps->__ptr[i].lpId);
-				else
-					strQuery += "null";
-
-				strQuery += ",";
-
-				if (lpsNamedProps->__ptr[i].lpString)
-					strQuery += "'" + strEscapedString + "'";
-				else
-					strQuery += "null";
-
-				strQuery += ",";
-
-				if (lpsNamedProps->__ptr[i].lpguid)
-					strQuery += strEscapedGUID;
-				else
-					strQuery += "null";
-
-				strQuery += ")";
-
-				er = lpDatabase->DoInsert(strQuery, &ulLastId);
-				if(er != erSuccess)
-					goto exit;
-
-				lpsResponse->lpsPropTags.__ptr[i] = ulLastId+1; // offset one because 0 is 'not found'
-			} else {
-				// No create ? Then not found
-				lpsResponse->lpsPropTags.__ptr[i] = 0;
+			if (lpsNamedProps->__ptr[i].lpguid == nullptr ||  lpDBRow[3] == nullptr ||
+			    memcmp(lpsNamedProps->__ptr[i].lpguid->__ptr, lpDBRow[3], lpsNamedProps->__ptr[i].lpguid->__size) != 0)
+				continue;
+			if ((nameid.size() > 0 && lpDBRow[1] && nameid.compare(lpDBRow[1]) == 0) ||
+			    (namestring.size() > 0 && lpDBRow[2] && namestring.compare(lpDBRow[2]) == 0)) {
+				lpsResponse->lpsPropTags.__ptr[i] = atoi(lpDBRow[0]) + 1;
+				break;
 			}
-		} else {
-			// found it
-			lpDBRow = lpDBResult.fetch_row();
-			if(lpDBRow!= NULL && lpDBRow[0] != NULL)
-				lpsResponse->lpsPropTags.__ptr[i] = atoi(lpDBRow[0])+1;
-			else
-				lpsResponse->lpsPropTags.__ptr[i] = 0;
 		}
 	}
 
+	if (!(ulFlags & MAPI_CREATE))
+		goto done;
+	er = lpDatabase->Begin();
+	if (er != erSuccess)
+		goto exit;
+
+	for (gsoap_size_t i = 0; i < lpsNamedProps->__size; ++i) {
+		if (lpsResponse->lpsPropTags.__ptr[i] != 0)
+			continue;
+		if (lpsNamedProps->__ptr[i].lpguid == nullptr) {
+			er = KCERR_NO_ACCESS;
+			goto exit;
+		}
+
+		strQuery = "INSERT INTO names (nameid, namestring, guid) VALUES(";
+		if (lpsNamedProps->__ptr[i].lpId != 0)
+			strQuery += stringify(*lpsNamedProps->__ptr[i].lpId);
+		else
+			strQuery += "null";
+
+		strQuery += ",";
+		if (lpsNamedProps->__ptr[i].lpString != nullptr) {
+			strEscapedString = lpDatabase->Escape(lpsNamedProps->__ptr[i].lpString);
+			strQuery += "'" + strEscapedString + "'";
+		} else {
+			strQuery += "null";
+		}
+
+		strQuery += ",";
+		strEscapedGUID = lpDatabase->EscapeBinary(lpsNamedProps->__ptr[i].lpguid->__ptr, lpsNamedProps->__ptr[i].lpguid->__size);
+		strQuery += strEscapedGUID;
+		strQuery += ")";
+		er = lpDatabase->DoInsert(strQuery, &ulLastId);
+		if (er != erSuccess)
+			goto exit;
+		lpsResponse->lpsPropTags.__ptr[i] = ulLastId+1; // offset one because 0 is 'not found'
+	}
+
+	er = lpDatabase->Commit();
+	if (er != erSuccess)
+		goto exit;
+done:
 	// Everything is done, now set the size
 	lpsResponse->lpsPropTags.__size = lpsNamedProps->__size;
 
-	er = lpDatabase->Commit();
-	if(er != erSuccess)
-		goto exit;
 exit:
 	ROLLBACK_ON_ERROR();
 }

commit 45929938c7e2cf9d4077638941d3fe1ec28400c7

libserver: handle duplicate named property requests
Asking for a named property more than once ought to yield (0,0,0) on
not found, e.g. (2,2,2) when found, and (2,2,2) when
flags=MAPI_CREATE:

	p = MAPINAMEID(PSETID_Archive, MNID_STRING, u'store-entryids')
	mapiobj.GetIDsFromNames([p, p, p], flags)

Before b3bf36fb, each element was handled individually with an SQL
query. If an INSERT happened because of flags=MAPI_CREATE, all
subsequent SELECTs would see it. b3bf36fb broke this two-fold: it
would return (2,0,0) when found, and (2,3,4) with MAPI_CREATE.

Fixes: kopanocore-8.4.0-freeze~278^2
References: KC-1107, KC-1108

provider/libserver/cmd.cpp
@@ -4337,6 +4337,17 @@ SOAP_ENTRY_START(getOwner, lpsResponse->er, entryId sEntryId, struct getOwnerRes
 }
 SOAP_ENTRY_END()
 
+static bool soap_namedprop_eq(const namedProp &p, const namedProp &q)
+{
+	if (p.lpguid == nullptr || q.lpguid == nullptr)
+		return false;
+	if (p.lpId != nullptr && q.lpId != nullptr)
+		return *p.lpId == *q.lpId;
+	if (p.lpString != nullptr && q.lpString != nullptr)
+		return strcasecmp(p.lpString, q.lpString) == 0;
+	return false;
+}
+
 SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNamedProps, unsigned int ulFlags, struct getIDsFromNamesResponse *lpsResponse)
 {
 	std::string		strEscapedString;
@@ -4409,10 +4420,8 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 			    memcmp(lpsNamedProps->__ptr[i].lpguid->__ptr, lpDBRow[3], lpsNamedProps->__ptr[i].lpguid->__size) != 0)
 				continue;
 			if ((nameid.size() > 0 && lpDBRow[1] && nameid.compare(lpDBRow[1]) == 0) ||
-			    (namestring.size() > 0 && lpDBRow[2] && namestring.compare(lpDBRow[2]) == 0)) {
+			    (namestring.size() > 0 && lpDBRow[2] && namestring.compare(lpDBRow[2]) == 0))
 				lpsResponse->lpsPropTags.__ptr[i] = tag;
-				break;
-			}
 		}
 	}
 
@@ -4433,7 +4442,7 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 		}
 
 		strQuery = "INSERT INTO names (nameid, namestring, guid) VALUES(";
-		if (lpsNamedProps->__ptr[i].lpId != 0)
+		if (lpsNamedProps->__ptr[i].lpId != nullptr)
 			strQuery += stringify(*lpsNamedProps->__ptr[i].lpId);
 		else
 			strQuery += "null";
@@ -4453,7 +4462,14 @@ SOAP_ENTRY_START(getIDsFromNames, lpsResponse->er,  struct namedPropArray *lpsNa
 		er = lpDatabase->DoInsert(strQuery, &ulLastId);
 		if (er != erSuccess)
 			goto exit;
-		lpsResponse->lpsPropTags.__ptr[i] = ulLastId+1; // offset one because 0 is 'not found'
+		/* Client might have requested the same name more than once */
+		for (gsoap_size_t j = i; j < lpsNamedProps->__size; ++j) {
+			if (lpsResponse->lpsPropTags.__ptr[j] != 0)
+				continue;
+			if (!soap_namedprop_eq(lpsNamedProps->__ptr[i], lpsNamedProps->__ptr[j]))
+				continue;
+			lpsResponse->lpsPropTags.__ptr[j] = ulLastId + 1; // offset one because 0 is 'not found'
+		}
 	}
 
 	er = lpDatabase->Commit();