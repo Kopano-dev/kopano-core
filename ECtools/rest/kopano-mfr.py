97ddf84323c9cb51c9c9440d0907fca31ddc47e4
Mark Dufour <m.dufour@kopano.com>
2018-03-30 12:37:14 +0200

    kopano-mfr: add logging
    Add logging.

--- ECtools/rest/kopano-mfr.py
+++ ECtools/rest/kopano-mfr.py
@@ -1,10 +1,14 @@
-from multiprocessing import Process
+import logging
+from logging.handlers import QueueHandler, QueueListener
+import multiprocessing
 import optparse
 import os
 import os.path
 import signal
 import sys
 import time
+
+import falcon
 
 try:
     import setproctitle
@@ -51,31 +55,71 @@ def opt_args():
         sys.exit(-1)
     return options, args
 
+def error_handler(ex, req, resp, params):
+    # Ignore falcon.HTTPError and re-raise
+    if isinstance(ex, falcon.HTTPError):
+        raise
+
+    # Do things with other errors here
+    logging.exception(ex)
+
+
 def run_app(socket_path, n, options):
     if SETPROCTITLE:
         setproctitle.setproctitle('kopano-mfr rest %d' % n)
     app = kopano_rest.RestAPI(options)
+    app.add_error_handler(Exception, error_handler)
     unix_socket = 'unix:' + os.path.join(socket_path, 'rest%d.sock' % n)
-    bjoern.run(app, unix_socket)
+    logging.info('starting rest worker: %s', unix_socket)
+    try:
+        bjoern.run(app, unix_socket)
+    except KeyboardInterrupt:
+        pass
 
 def run_notify(socket_path, options):
+    # TODO merge with run_app?
     if SETPROCTITLE:
         setproctitle.setproctitle('kopano-mfr notify')
     app = kopano_rest.NotifyAPI(options)
+    app.add_error_handler(Exception, error_handler)
     unix_socket = 'unix:' + os.path.join(socket_path, 'notify.sock')
-    bjoern.run(app, unix_socket)
+    logging.info('starting notify worker: %s', unix_socket)
+    try:
+        bjoern.run(app, unix_socket)
+    except KeyboardInterrupt:
+        pass
+
+def logger_init():
+    q = multiprocessing.Queue()
+    # this is the handler for all log records
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(levelname)s: %(asctime)s - %(process)s - %(message)s"))
+
+    # ql gets records from the queue and sends them to the handler
+    ql = QueueListener(q, handler)
+    ql.start()
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    # add the handler to the logger so records from this process are handled
+    logger.addHandler(handler)
+
+    return ql, q
 
 def main():
     options, args = opt_args()
     socket_path = options.socket_path or SOCKET_PATH
     nworkers = options.workers if options.workers is not None else WORKERS
 
+    q_listener, q = logger_init()
+    logging.info('starting kopano-mfr')
+
     workers = []
     for n in range(nworkers):
-        process = Process(target=run_app, args=(socket_path, n, options))
+        process = multiprocessing.Process(target=run_app, args=(socket_path, n, options))
         workers.append(process)
 
-    notify_process = Process(target=run_notify, args=(socket_path, options))
+    notify_process = multiprocessing.Process(target=run_notify, args=(socket_path, options))
     workers.append(notify_process)
 
     for worker in workers:
@@ -89,6 +133,8 @@ def main():
         for worker in workers:
             worker.terminate()
     finally:
+        q_listener.stop()
+
         sockets = []
         for n in range(nworkers):
             sockets.append('rest%d.sock' % n)
