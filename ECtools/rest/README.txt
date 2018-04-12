215fdb6994a9c2701b661cc9fa1feb2d960fd682
Mark Dufour <m.dufour@kopano.com>
2018-04-12 13:02:37 +0200

    rest: flesh out __main__ further
    Flesh out __main__ further, including notification process.

--- ECtools/rest/README.txt
+++ ECtools/rest/README.txt
@@ -3,14 +3,14 @@ STARTING
 in a production environment, kopano-rest is deployed in combination with
 konnect, kopano-apid and the provided kopano-mfr script.
 
-for convenience, it's possible to start kopano-rest as follows (no
-notification/subscription support as of now):
+for convenience, it's possible to start kopano-rest as follows:
 
-python3 -m kopano_rest
+python3 -m kopano_rest [REST_PORT=8000, NOTIFY_PORT=8001]
 
 or as follows:
 
 gunicorn3 'kopano_rest:RestAPI()'
+gunicorn3 'kopano_rest:NotifyAPI()'
 
 to profile requests (output in kopano.pstats):
 
--- ECtools/rest/kopano_rest/__main__.py
+++ ECtools/rest/kopano_rest/__main__.py
@@ -1,7 +1,18 @@
-from .api_v0.rest import RestAPIv0
+import multiprocessing
+import sys
 from wsgiref.simple_server import make_server
 
-app = RestAPIv0()
+from .api_v0.rest import RestAPIv0
+from .api_v0.notify import NotifyAPIv0
 
-s = make_server('localhost', 8000, app)
-s.serve_forever()
+REST_PORT = int(sys.argv[1]) if len(sys.argv) > 2 else 8000
+NOTIFY_PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 8001
+
+def run_rest():
+    make_server('localhost', REST_PORT, RestAPIv0()).serve_forever()
+
+def run_notify():
+    make_server('localhost', NOTIFY_PORT, NotifyAPIv0()).serve_forever()
+
+multiprocessing.Process(target=run_rest).start()
+multiprocessing.Process(target=run_notify).start()
