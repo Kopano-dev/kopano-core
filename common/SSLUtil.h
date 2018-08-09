d14eb68d3ac18773f9fd22e4e7cec5a091b27bb5
Jan Engelhardt <jengelh@inai.de>
2018-08-09 12:19:18 +0200

    common: disable TLSv1.0, 1.1 by default
    RHEL 6 with openssl 1.0.1e can do TLSv1.2.
    zarafa6client.dll 7.2.6 can do TLSv1.2.
    As for Android, [1] says 5.0 (and varying results on 4.x).
    
    [1] https://blog.dev-area.net/2015/08/13/android-4-1-enable-tls-1-1-and-tls-1-2/

--- common/SSLUtil.h
+++ common/SSLUtil.h
@@ -17,8 +17,8 @@ extern _kc_export void SSL_library_cleanup(void);
 extern _kc_export void ssl_random_init(void);
 extern _kc_export void ssl_random(bool b64bit, uint64_t *out);
 
-#define KC_DEFAULT_SSLPROTOLIST "!SSLv2 !SSLv3"
-#define KC_DEFAULT_CIPHERLIST "DEFAULT:!LOW:!SSLv2:!SSLv3:!EXPORT:!DH:!PSK:!kRSA:!aDSS:!aNULL"
+#define KC_DEFAULT_SSLPROTOLIST "!SSLv2 !SSLv3 !TLSv1.0 !TLSv1.1"
+#define KC_DEFAULT_CIPHERLIST "DEFAULT:!LOW:!SSLv2:!SSLv3:!TLSv1.0:!TLSv1.1:!EXPORT:!DH:!PSK:!kRSA:!aDSS:!aNULL"
 #ifdef NID_X25519
 #	define KC_DEFAULT_ECDH_CURVES "X25519:P-521:P-384:P-256"
 #else
