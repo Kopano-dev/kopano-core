a3e2f7d268caa7bfc2d9381b12a72f37a2335287
Jan Engelhardt <jengelh@inai.de>
2018-09-11 11:47:57 +0200

    libserver: re-work ALTER statements for utf8mb4 conversion
    Unspecific reports suggested that there might be some past MySQL
    version which is unable to see that an ALTER TABLE CONVERT TO statment
    on a table without any CHAR-based table fields is a conversion no-op.
    
    Those tables only need an ALTER TABLE DEFAULT CHARSET, so just do
    that.
    
    References: KC-725

--- provider/libserver/ECDBDef.h
+++ provider/libserver/ECDBDef.h
@@ -329,9 +329,9 @@
  * version that can be reached with creates only.
  * (This is never less than %Z_UPDATE_LAST.)
  */
-#define Z_UPDATE_RELEASE_ID 112
+#define Z_UPDATE_RELEASE_ID 115
 
 // This is the last update ID always update this to the last ID
-#define Z_UPDATE_LAST 112
+#define Z_UPDATE_LAST 115
 
 #endif
