2eb05133feb24792838c3789ae23398375d10fa4
Felix Bartels <f.bartels@kopano.com>
2018-10-04 14:14:33 +0200

    presence: fix default value for plugins
    References: KC-1057
    
    Signed-off-by: Felix Bartels <f.bartels@kopano.com>

--- ECtools/presence/kopano_presence/__init__.py
+++ ECtools/presence/kopano_presence/__init__.py
@@ -21,7 +21,7 @@ from flask import Flask, request, abort
 CONFIG = {
     'data_path': Config.string(default='/var/lib/kopano/presence/'),
     'data_save_interval': Config.integer(default=5),
-    'plugins': Config.string(multiple=True, default="spreed"),
+    'plugins': Config.string(multiple=True, default=['spreed']),
     'run_as_user': Config.string(default="kopano"),
     'run_as_group': Config.string(default="kopano"),
     'server_bind': Config.string(default="127.0.0.1"),
