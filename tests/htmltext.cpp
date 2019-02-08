f874e93dcb183cf2cc41e04b81cfa6af110f3c86
Jan Engelhardt <jengelh@inai.de>
2019-02-08 23:23:09 +0100

    htmltext: add missing error handling

--- tests/htmltext.cpp
+++ tests/htmltext.cpp
@@ -11,17 +11,21 @@ int main(int argc, char **argv)
 	std::wstring line, html;
 	CHtmlToTextParser parser;
 
-	if (argc < 2)
+	if (argc < 2) {
 		std::cerr << "Usage: requires a HTML file\n";
-
+		return EXIT_FAILURE;
+	}
 	std::wifstream htmlfile(argv[1]);
-	if (!htmlfile.is_open())
+	if (!htmlfile.is_open()) {
 		std::cerr << "Unable to open \"" << argv[1] << "\"\n";
+		return EXIT_FAILURE;
+	}
 	while (std::getline(htmlfile, line))
 		html += line;
-	if (!parser.Parse(html.c_str()))
+	if (!parser.Parse(html.c_str())) {
 		std::cerr << "Unable to parse HTML\n";
-
+		return EXIT_FAILURE;
+	}
 	std::wcout << parser.GetText();
 	return EXIT_SUCCESS;
 }
