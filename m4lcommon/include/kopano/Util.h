9756680dc17ba248604b27867db1c67327be6d9b
Jan Engelhardt <jengelh@inai.de>
2019-02-23 09:59:26 +0100

    m4lcommon: turn KC::Util into a namespace

--- m4lcommon/include/kopano/Util.h
+++ m4lcommon/include/kopano/Util.h
@@ -17,64 +17,58 @@
 
 namespace KC {
 
-/*
- * This class only exists to satisfy the %extend directive in swig/libcommon.i.
- * It screams for replacement with a namespace Util {}
- */
-class Util KC_FINAL {
-	public:
-	_kc_export static HRESULT HrAddToPropertyArray(const SPropValue *src, ULONG srcvals, const SPropValue *add, SPropValue **dest, ULONG *ndestvals);
-	_kc_export static HRESULT HrMergePropertyArrays(const SPropValue *src, ULONG srcvals, const SPropValue *adds, ULONG naddvals, SPropValue **dest, ULONG *destvals);
-	_kc_export static HRESULT HrCopyPropertyArray(const SPropValue *src, ULONG srcvals, LPSPropValue *dest, ULONG *destvals, bool excl_errors = false);
-	static HRESULT	HrCopyPropertyArrayByRef(const SPropValue *lpSrc, ULONG cValues, LPSPropValue *lppDest, ULONG *cDestValues, bool bExcludeErrors = false);
-	static HRESULT	HrCopyPropertyArray(const SPropValue *lpSrc, ULONG cValues, LPSPropValue lpDest, void *lpBase);
-	static HRESULT	HrCopyPropertyArrayByRef(const SPropValue *lpSrc, ULONG cValues, LPSPropValue lpDest);
-	_kc_export static HRESULT HrCopyProperty(LPSPropValue dest, const SPropValue *src, void *base, ALLOCATEMORE * = nullptr);
-	static HRESULT	HrCopyPropertyByRef(LPSPropValue lpDest, const SPropValue *lpSrc);
-	_kc_export static HRESULT HrCopySRestriction(LPSRestriction dst, const SRestriction *src, void *base);
-	_kc_export static HRESULT  HrCopySRestriction(LPSRestriction *dst, const SRestriction *src);
-	static HRESULT	HrCopySRowSet(LPSRowSet lpDest, const SRowSet *lpSrc, void *lpBase);
-	_kc_export static HRESULT HrCopySRow(LPSRow dest, const SRow *src, void *base);
-	_kc_export static HRESULT HrCopyPropTagArray(const SPropTagArray *src, LPSPropTagArray *dst);
-	_kc_export static void proptag_change_unicode(ULONG flags, SPropTagArray &);
-	_kc_export static HRESULT HrCopyBinary(ULONG size, const BYTE *src, ULONG *destsize, LPBYTE *dest, LPVOID lpBase = nullptr);
-	_kc_export static HRESULT HrCopyEntryId(ULONG size, const ENTRYID *src, ULONG *destsize, LPENTRYID *dest, LPVOID base = nullptr);
-	_kc_export static int CompareSBinary(const SBinary &, const SBinary &);
-	_kc_export static HRESULT CompareProp(const SPropValue *, const SPropValue *, const ECLocale &, int *res);
-	static unsigned int PropSize(const SPropValue *lpProp);
-	_kc_export static LONG FindPropInArray(const SPropTagArray *proptags, ULONG tag);
-	_kc_export static HRESULT HrStreamToString(IStream *in, std::string &out);
-	_kc_export static HRESULT HrStreamToString(IStream *in, std::wstring &out);
-	_kc_export static HRESULT HrTextToRtf(IStream *text, IStream *rtf);
-	_kc_export static HRESULT HrTextToHtml(IStream *text, IStream *html, ULONG codepage);
-	_kc_export static HRESULT HrTextToHtml(const wchar_t *text, std::string &html, ULONG codepage);
-	_kc_export static HRESULT HrHtmlToText(IStream *html, IStream *text, ULONG codepage);
-	_kc_export static HRESULT HrHtmlToRtf(IStream *html, IStream *rtf, unsigned int codepage);
-	static HRESULT	HrHtmlToRtf(const WCHAR *lpwHTML, std::string& strRTF);
+namespace Util {
 
-	static ULONG GetBestBody(const SPropValue *lpBody, const SPropValue *lpHtml, const SPropValue *lpRtfCompressed, const SPropValue *lpRtfInSync, ULONG ulFlags);
-	_kc_export static ULONG GetBestBody(IMAPIProp *obj, ULONG flags);
-	_kc_export static ULONG GetBestBody(LPSPropValue props, ULONG nvals, ULONG flags);
-	_kc_export static bool IsBodyProp(ULONG tag);
-	_kc_export static HRESULT HrMAPIErrorToText(HRESULT, LPTSTR *err, void *base = nullptr);
-	_kc_export static bool ValidatePropTagArray(const SPropTagArray *);
-	_kc_export static HRESULT hex2bin(const char *input, size_t len, ULONG *outlen, LPBYTE *output, void *parent = nullptr);
-	static HRESULT hex2bin(const char *input, size_t len, LPBYTE output);
+extern _kc_export HRESULT HrAddToPropertyArray(const SPropValue *src, unsigned int srcvals, const SPropValue *add, SPropValue **dest, unsigned int *ndestvals);
+extern _kc_export HRESULT HrMergePropertyArrays(const SPropValue *src, unsigned int srcvals, const SPropValue *adds, unsigned int naddvals, SPropValue **dest, unsigned int *destvals);
+extern _kc_export HRESULT HrCopyPropertyArray(const SPropValue *src, unsigned int srcvals, SPropValue **dest, unsigned int *destvals, bool excl_errors = false);
+extern HRESULT HrCopyPropertyArrayByRef(const SPropValue *src, unsigned int srcvals, SPropValue **dest, unsigned int *destvals, bool excl_errors = false);
+extern HRESULT HrCopyPropertyArray(const SPropValue *src, unsigned int srcvals, SPropValue *dest, void *base);
+extern HRESULT HrCopyPropertyArrayByRef(const SPropValue *src, unsigned int srcvals, SPropValue *dest);
+extern _kc_export HRESULT HrCopyProperty(SPropValue *dest, const SPropValue *src, void *base, ALLOCATEMORE * = nullptr);
+extern HRESULT HrCopyPropertyByRef(SPropValue *dest, const SPropValue *src);
+extern _kc_export HRESULT HrCopySRestriction(SRestriction *dst, const SRestriction *src, void *base);
+extern _kc_export HRESULT  HrCopySRestriction(SRestriction **dst, const SRestriction *src);
+extern HRESULT HrCopySRowSet(SRowSet *dest, const SRowSet *src, void *base);
+extern _kc_export HRESULT HrCopySRow(SRow *dest, const SRow *src, void *base);
+extern _kc_export HRESULT HrCopyPropTagArray(const SPropTagArray *src, SPropTagArray **dst);
+extern _kc_export void proptag_change_unicode(unsigned int flags, SPropTagArray &);
+extern _kc_export HRESULT HrCopyBinary(unsigned int size, const BYTE *src, unsigned int *destsize, BYTE **dest, void *base = nullptr);
+extern _kc_export HRESULT HrCopyEntryId(unsigned int size, const ENTRYID *src, unsigned int *destsize, ENTRYID **dest, void *base = nullptr);
+extern _kc_export int CompareSBinary(const SBinary &, const SBinary &);
+extern _kc_export HRESULT CompareProp(const SPropValue *, const SPropValue *, const ECLocale &, int *res);
+extern unsigned int PropSize(const SPropValue *);
+extern _kc_export int FindPropInArray(const SPropTagArray *proptags, unsigned int tag);
+extern _kc_export HRESULT HrStreamToString(IStream *in, std::string &out);
+extern _kc_export HRESULT HrStreamToString(IStream *in, std::wstring &out);
+extern _kc_export HRESULT HrTextToRtf(IStream *text, IStream *rtf);
+extern _kc_export HRESULT HrTextToHtml(IStream *text, IStream *html, unsigned int codepage);
+extern _kc_export HRESULT HrTextToHtml(const wchar_t *text, std::string &html, unsigned int codepage);
+extern _kc_export HRESULT HrHtmlToText(IStream *html, IStream *text, unsigned int codepage);
+extern _kc_export HRESULT HrHtmlToRtf(IStream *html, IStream *rtf, unsigned int codepage);
+extern HRESULT HrHtmlToRtf(const wchar_t *whtml, std::string &rtf);
+extern unsigned int GetBestBody(const SPropValue *body, const SPropValue *html, const SPropValue *rtfcomp, const SPropValue *rtfinsync, unsigned int flags);
+extern _kc_export unsigned int GetBestBody(IMAPIProp *obj, unsigned int flags);
+extern _kc_export unsigned int GetBestBody(SPropValue *props, unsigned int nvals, unsigned int flags);
+extern _kc_export bool IsBodyProp(unsigned int tag);
+extern _kc_export HRESULT HrMAPIErrorToText(HRESULT, TCHAR **err, void *base = nullptr);
+extern _kc_export bool ValidatePropTagArray(const SPropTagArray *);
+extern _kc_export HRESULT hex2bin(const char *input, size_t len, unsigned int *outlen, BYTE **output, void *parent = nullptr);
+extern HRESULT hex2bin(const char *input, size_t len, BYTE *output);
+extern _kc_export HRESULT CopyAttachments(IMessage *src, IMessage *dst, SRestriction *r);
+extern _kc_export HRESULT DoCopyTo(const IID *src_intf, void *src_obj, unsigned int ciidExclude, const IID *rgiidExclude, const SPropTagArray *exclprop, unsigned int ui_param, IMAPIProgress *, const IID *dst_intf, void *dst_obj, unsigned int flags, SPropProblemArray **);
+extern _kc_export HRESULT DoCopyProps(const IID *src_intf, void *src_obj, const SPropTagArray *inclprop, unsigned int ui_param, IMAPIProgress *, const IID *dst_intf, void *dst_obj, unsigned int flags, SPropProblemArray **);
+extern _kc_export HRESULT HrCopyIMAPData(IMessage *src, IMessage *dst);
+extern _kc_export HRESULT HrDeleteIMAPData(IMessage *);
+extern _kc_export HRESULT HrGetQuotaStatus(IMsgStore *, ECQUOTA *, ECQUOTASTATUS **ret);
+extern _kc_export HRESULT HrDeleteAttachments(IMessage *);
+extern _kc_export HRESULT HrDeleteMessage(IMAPISession *, IMessage *);
+extern _kc_export HRESULT ReadProperty(IMAPIProp *, unsigned int tag, std::string &data);
+extern _kc_export HRESULT WriteProperty(IMAPIProp *, unsigned int tag, const std::string &data);
+extern _kc_export HRESULT ExtractSuggestedContactsEntryID(SPropValue *prop_blob, unsigned int *eid_size, ENTRYID **eid);
+extern HRESULT ExtractAdditionalRenEntryID(SPropValue *prop_blob, unsigned short block_type, unsigned int *eid_size, ENTRYID **eid);
 
-	/* DoCopyTo/DoCopyProps functions & their helpers */
-	_kc_export static HRESULT CopyAttachments(LPMESSAGE src, LPMESSAGE dst, LPSRestriction r);
-	_kc_export static HRESULT DoCopyTo(LPCIID src_intf, LPVOID src_obj, ULONG ciidExclude, LPCIID rgiidExclude, const SPropTagArray *exclprop, ULONG ui_param, LPMAPIPROGRESS, LPCIID dst_intf, LPVOID dst_obj, ULONG flags, LPSPropProblemArray *);
-	_kc_export static HRESULT DoCopyProps(LPCIID src_intf, LPVOID src_obj, const SPropTagArray *inclprop, ULONG ui_param, LPMAPIPROGRESS, LPCIID dst_intf, LPVOID dst_obj, ULONG flags, LPSPropProblemArray *);
-	_kc_export static HRESULT HrCopyIMAPData(LPMESSAGE src, LPMESSAGE dst);
-	_kc_export static HRESULT HrDeleteIMAPData(LPMESSAGE);
-	_kc_export static HRESULT HrGetQuotaStatus(IMsgStore *, ECQUOTA *, ECQUOTASTATUS **ret);
-	_kc_export static HRESULT HrDeleteAttachments(LPMESSAGE);
-	_kc_export static HRESULT HrDeleteMessage(IMAPISession *, IMessage *);
-	_kc_export static HRESULT ReadProperty(IMAPIProp *, ULONG tag, std::string &data);
-	_kc_export static HRESULT WriteProperty(IMAPIProp *, ULONG tag, const std::string &data);
-	_kc_export static HRESULT ExtractSuggestedContactsEntryID(LPSPropValue prop_blob, ULONG *eid_size, LPENTRYID *eid);
-	static HRESULT ExtractAdditionalRenEntryID(LPSPropValue lpPropBlob, unsigned short usBlockType, ULONG *lpcbEntryID, LPENTRYID *lppEntryID);
-};
+} /* namespace Util */
 
 #define RTF_TAG_TYPE_TEXT	0x0000
 #define RTF_TAG_TYPE_HTML	0x0010
