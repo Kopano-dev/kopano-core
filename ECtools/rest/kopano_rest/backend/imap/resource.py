0aaf55fd13421a77ed9c92b4fa2ed8cc0591f371
Simon Eisenmann <s.eisenmann@kopano.com>
2019-03-14 16:46:18 +0100

    rest: remove rest as it has moved to its own tree
    The rest subpackage is now available on its own terms as a separate
    project at https://stash.kopano.io/projects/KC/repos/grapi and thus
    is removed from kopanocore.

--- ECtools/rest/kopano_rest/backend/imap/resource.py
+++
@@ -1,5 +0,0 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-class Resource(object):
-    def __init__(self, options):
-        self.options = options
--- ECtools/rest/kopano_rest/backend/ldap/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-from .resource import Resource
-from .user import UserResource
-
-GroupResource = Resource
-ContactFolderResource = Resource
-ContactResource = Resource
-ProfilePhotoResource = Resource
