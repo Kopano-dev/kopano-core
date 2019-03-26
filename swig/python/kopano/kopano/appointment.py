6aeb564301e8f95e20f20950341a854b72480e31
Mark Dufour <m.dufour@kopano.com>
2019-03-26 10:09:34 +0100

    pyko: cleanup appointment.py

--- swig/python/kopano/kopano/appointment.py
+++ swig/python/kopano/kopano/appointment.py
@@ -3,7 +3,7 @@
 Part of the high-level python bindings for Kopano
 
 Copyright 2005 - 2016 Zarafa and its licensors (see LICENSE file)
-Copyright 2016 - Kopano and its licensors (see LICENSE file)
+Copyright 2016 - 2019 Kopano and its licensors (see LICENSE file)
 """
 import sys
 
@@ -28,8 +28,8 @@ from .compat import (
     benc as _benc, bdec as _bdec, fake_unicode as _unicode,
 )
 from .defs import (
-    PSETID_Appointment, ASF_CANCELED, NR_COLOR, COLOR_NR, FB_STATUS, STATUS_FB,
-    ASF_MEETING,
+    PSETID_Appointment, ASF_CANCELED, NR_COLOR, COLOR_NR, FB_STATUS,
+    STATUS_FB, ASF_MEETING,
 )
 from .pidlid import (
     PidLidReminderSet, PidLidReminderDelta, PidLidAppointmentSubType,
@@ -37,6 +37,7 @@ from .pidlid import (
     PidLidTimeZoneStruct, PidLidTimeZoneDescription, PidLidLocation,
     PidLidAppointmentStateFlags, PidLidAppointmentColor, PidLidResponseStatus,
 )
+
 try:
     from . import utils as _utils
 except ImportError: # pragma: no cover
@@ -44,17 +45,23 @@ except ImportError: # pragma: no cover
 
 from . import timezone as _timezone
 
+# TODO use pidlids instead!
 ALL_DAY_NAME = (PSETID_Appointment, MNID_ID, 0x8215)
-START_NAME = (PSETID_Appointment, MNID_ID, 33293) # TODO use pidlid instead
+START_NAME = (PSETID_Appointment, MNID_ID, 33293)
 END_NAME = (PSETID_Appointment, MNID_ID, 33294)
 RECURRING_NAME = (PSETID_Appointment, MNID_ID, 33315)
 BUSYSTATUS = (PSETID_Appointment, MNID_ID, 33285)
 
 class Appointment(object):
-    """Appointment mixin class"""
+    """Appointment (mixin) class
+
+    Appointment-specific functionality, mixed into the :class:`items <Item>`
+    class. Only used to separate out this functionality.
+    """
 
     @property
     def all_day(self):
+        """Appointment is all-day."""
         proptag = self.store._name_id(ALL_DAY_NAME) | PT_BOOLEAN
         return self._get_fast(proptag)
 
@@ -63,46 +70,55 @@ class Appointment(object):
         self[PidLidAppointmentSubType] = value
 
     @property
-    def show_as(self):
+    def busy_status(self): # TODO setter
+        """Appointment busy status (free, tentative, busy, out_of_office,
+        working_elsewhere or unknown)"""
         try:
             return {
-                0: u'free',
-                1: u'tentative',
-                2: u'busy',
-                3: u'out_of_office',
-                4: u'working_elsewhere'
+                0: 'free',
+                1: 'tentative',
+                2: 'busy',
+                3: 'out_of_office',
+                4: 'working_elsewhere'
             }[self[PidLidBusyStatus]]
         except NotFoundError:
-            return u'unknown'
+            return 'unknown'
+
+    @property
+    def show_as(self): # TODO deprecate?
+        return self.busy_status
 
     @property
     def start(self):
+        """Appointment start."""
         proptag = self.store._name_id(START_NAME) | PT_SYSTIME
         return self._get_fast(proptag)
 
     @start.setter
     def start(self, val): # TODO update/invalidate cache
-        # XXX check if exists?
-        self.create_prop('common:34070', val, PT_SYSTIME) # props are identical
+        # TODO check if exists?
+        self.create_prop('common:34070', val, PT_SYSTIME) # props identical
         self.create_prop('appointment:33293', val, PT_SYSTIME)
         if self.recurring:
             self.recurrence._update_offsets()
 
     @property
     def end(self):
+        """Appointment end."""
         proptag = self.store._name_id(END_NAME) | PT_SYSTIME
         return self._get_fast(proptag)
 
     @end.setter
     def end(self, val): # TODO update/invalidate cache
-        # XXX check if exists?
-        self.create_prop('common:34071', val, PT_SYSTIME) # props are identical
+        # TODO check if exists?
+        self.create_prop('common:34071', val, PT_SYSTIME) # props identical
         self.create_prop('appointment:33294', val, PT_SYSTIME)
         if self.recurring:
             self.recurrence._update_offsets()
 
     @property
     def location(self):
+        """Appointment location."""
         return self.get(PidLidLocation)
 
     @location.setter
@@ -111,6 +127,7 @@ class Appointment(object):
 
     @property
     def recurring(self):
+        """Appointment is recurring."""
         proptag = self.store._name_id(RECURRING_NAME) | PT_BOOLEAN
         return self._get_fast(proptag)
 
@@ -122,23 +139,31 @@ class Appointment(object):
         self[PidLidRecurring] = value
 
     @property
-    def busystatus(self):
+    def busystatus(self): # TODO deprecate and use busy_status instead?
         proptag = self.store._name_id(BUSYSTATUS) | PT_LONG
         return FB_STATUS.get(self._get_fast(proptag))
 
     @busystatus.setter
     def busystatus(self, val):
         try:
-            self.create_prop('appointment:33285', STATUS_FB[val], PT_LONG) # props are identical
+            # props are identical
+            self.create_prop('appointment:33285', STATUS_FB[val], PT_LONG)
         except KeyError:
             raise ArgumentError('invalid busy status: %s' % val)
 
 
     @property
     def recurrence(self):
+        """Appointment :class:`recurrence <Recurrence>`."""
         return Recurrence(self)
 
     def occurrences(self, start=None, end=None):
+        """Appointment :class:`occurrences <Occurrence>` (expanding
+        recurring appointments).
+
+        :param start: start from given date (optional)
+        :param end: end at given date (optional)
+        """
         if self.recurring:
             for occ in self.recurrence.occurrences(start=start, end=end):
                 yield occ
@@ -158,7 +183,7 @@ class Appointment(object):
 
     @property
     def reminder(self):
-        """Is reminder set."""
+        """Is appointment reminder set."""
         return self.get(PidLidReminderSet, False)
 
     @reminder.setter
@@ -175,19 +200,26 @@ class Appointment(object):
         self[PidLidReminderDelta] = value
 
     @property
-    def rrule(self): # XXX including timezone!
-        if self.recurring: # XXX rrule for non-recurring makes sense?
+    def rrule(self): # TODO including timezone!
+        if self.recurring: # TODO rrule for non-recurring makes sense?
             return self.recurrence.recurrences
 
-    # XXX rrule setter!
+    # TODO rrule setter!
 
     # TODO merge with item.recipients?
     def attendees(self):
+        """Appointment :class:`attendees <Attendee>`."""
         for row in self.table(PR_MESSAGE_RECIPIENTS):
             yield Attendee(self.server, row)
 
     def create_attendee(self, type_, address):
+        """Create appointment :class:`attendee <Attendee>`.
+
+        :param type_: attendee type (required, optional or resource)
+        :param address: attendee address (str or :class:`address <Address>`)
+        """
         # TODO move to Attendee class
+        # TODO return Attendee instance
 
         reciptype = {
             'required': 1,
@@ -214,10 +246,12 @@ class Appointment(object):
 
     @property
     def response_requested(self):
+        """Is appointment response requested."""
         return self.get(PR_RESPONSE_REQUESTED, False)
 
     @property
     def icaluid(self):
+        """Appointment iCal UID."""
         try:
             return _benc(self[PidLidGlobalObjectId])
         except NotFoundError:
@@ -232,12 +266,14 @@ class Appointment(object):
 
     @property
     def tzinfo(self):
+        """Appointment timezone as datetime compatible tzinfo object."""
         tzdata = self.get(PidLidTimeZoneStruct)
         if tzdata:
             return _timezone.MAPITimezone(tzdata)
 
     @property
     def timezone(self):
+        """Appointment timezone description."""
         return self.get(PidLidTimeZoneDescription)
 
     @timezone.setter
@@ -270,8 +306,8 @@ class Appointment(object):
         response.subject = subject_prefix + ': ' + self.subject
         if comment:
             response.text = comment
-        response.to = self.server.user(email=self.from_.email) # XXX
-        response.from_ = self.store.user # XXX slow?
+        response.to = self.server.user(email=self.from_.email) # TODO
+        response.from_ = self.store.user # TODO slow?
 
         response.send()
 
@@ -280,10 +316,12 @@ class Appointment(object):
 
     @property
     def canceled(self):
+        """Is appointment canceled."""
         return bool(self[PidLidAppointmentStateFlags] & ASF_CANCELED)
 
     @property
     def color(self): # property used by old clients
+        """Appointment color (old clients)."""
         nr = self.get(PidLidAppointmentColor, 0)
         if nr != 0:
             return NR_COLOR[nr]
