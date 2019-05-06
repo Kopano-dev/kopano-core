da202127558a318873e4ff78fef980b554fb5dbd
Jan Engelhardt <jengelh@inai.de>
2019-05-06 12:06:40 +0200

    icalmapi: add summary for vcftomapi API

--- libicalmapi/vcftomapi.hpp
+++ libicalmapi/vcftomapi.hpp
@@ -8,9 +8,23 @@ namespace KC {
 
 class vcftomapi {
 	public:
+	/**
+	 * @o: a MAPI object to use for resolving named properties
+	 */
 	vcftomapi(IMAPIProp *o) : m_propobj(o) {}
 	virtual ~vcftomapi(void) = default;
+
+	/**
+	 * Parses the contents of a .vcf file and adds recognized VCARDs to the
+	 * internal buffer. Returns %MAPI_E_CORRUPT_DATA if no VCARDs were found.
+	 */
 	virtual HRESULT parse_vcf(const std::string &ical) = 0;
+
+	/**
+	 * Pops the next VCARD (contact) available in the internal buffer and
+	 * sets the given MAPI message's properties with the data.
+	 * Returns %MAPI_E_NOT_FOUND once no more VCARDs are available.
+	 */
 	virtual HRESULT get_item(IMessage *) = 0;
 
 	protected:
