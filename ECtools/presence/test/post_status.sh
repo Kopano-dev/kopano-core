64882f7050cb5f1a47ce8b64540f86098610609b
Simon Eisenmann <s.eisenmann@kopano.com>
2020-01-22 08:34:40 +0000

    presence: remove presence implementation
    Kopano presence is no longer needed with Kopano Meet. Thus the
    presence implementation can go away here since it is not
    directly related to groupware services anyways.
    
    Reference: KC-1679

--- ECtools/presence/test/post_status.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: AGPL-3.0-only
-curl -u presenca:presence -X GET -d '{"AuthenticationToken":"1421324321:markd@kopano.com:WFAI3NU3YBAA91F4+9BOILXYGTPAV+PVDRNKIJ+JCVY=", "Type": "UserStatus", "UserStatus": [{"user_id": "guido@psf.org"}, {"user_id": "rms@gnu.org"}, {"user_id": "markd"}, {"user_id": "ryan"}]}' http://localhost:1234/ -H "Content-Type: application/json"
