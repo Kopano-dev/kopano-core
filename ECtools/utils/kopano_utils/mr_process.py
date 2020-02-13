8caf98966a53fbe9ad4a40e5158c7931bf787627
Jelle van der Waa <j.vanderwaa@kopano.com>
2020-02-13 11:47:26 +0100

    ECtools: mr_process: use logging and extend logging
    Add more logging at the request of professional services to log if a
    meeting requests has been accepted by a delegate or the user itself.

--- ECtools/utils/kopano_utils/mr_process.py
+++ ECtools/utils/kopano_utils/mr_process.py
@@ -5,6 +5,22 @@ import sys
 
 import kopano
 
+
+def get_sender(item):
+    '''
+    Retrieves the sender from either PR_SENDER_* (item.sender) or when a
+    delegate has approved the meeting request on behalf of someone
+    the PR_SENT_REPRESENTING_* properties are used.
+    '''
+
+    if item.from_.entryid != item.sender.entryid:
+        fromname = "'{}' on behalf of '{}'".format(item.sender.name, item.from_.name)
+    else:
+        fromname = "'{}'".format(item.sender.name)
+
+    return fromname
+
+
 def main():
     username, config_file, entryid = sys.argv[1:]
 
@@ -16,18 +32,22 @@ def main():
     mr = item.meetingrequest
 
     if mr.is_request:
+        server.log.debug("Accepting meeting request tentative from %s", get_sender(item))
         mr.accept(tentative=True, response=False)
     elif mr.is_response:
+        server.log.debug("Processing meeting request response from %s", get_sender(item))
         mr.process_response()
     elif mr.is_cancellation:
+        server.log.debug("Processing meeting request cancellation from %s", get_sender(item))
         mr.process_cancellation()
 
     now = datetime.now()
 
     try:
+        server.log.debug("Updating freebusy")
         user.freebusy.publish(now - timedelta(7), now + timedelta(180))
     except kopano.errors.NotFoundError as e:
-        print("Unable to publish freebusy information: " + str(e), file=sys.stderr)
+        server.log.error("Unable to publish freebusy information: %s", str(e))
 
 if __name__ == '__main__':
     main() # pragma: no cover
