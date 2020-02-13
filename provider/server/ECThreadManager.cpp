31693256ed9edb8c0fc8e2c58c96a9407d3f25b1
Jan Engelhardt <jengelh@inai.de>
2020-02-13 16:33:00 +0100

    server: fix a case of dereference an end iterator
    valgrind complained about the line, add a guard for it.

--- provider/server/ECThreadManager.cpp
+++ provider/server/ECThreadManager.cpp
@@ -640,20 +640,22 @@ ECRESULT ECDispatcherEPoll::MainLoop()
 			} else {
 				// this is a new request from an existing client
 				auto iterSockets = m_setSockets.find(epevents[i].data.fd);
-				// remove from epfd, either close socket, or it will be reactivated later in the epfd
-				epevent.data.fd = iterSockets->second.soap->socket;
-				epoll_ctl(m_epFD, EPOLL_CTL_DEL, iterSockets->second.soap->socket, &epevent);
+				if (iterSockets != m_setSockets.cend()) {
+					// remove from epfd, either close socket, or it will be reactivated later in the epfd
+					epevent.data.fd = iterSockets->second.soap->socket;
+					epoll_ctl(m_epFD, EPOLL_CTL_DEL, iterSockets->second.soap->socket, &epevent);
 
-				if (epevents[i].events & EPOLLHUP) {
-					kopano_end_soap_connection(iterSockets->second.soap);
-					soap_free(iterSockets->second.soap);
-					m_setSockets.erase(iterSockets);
-				} else {
-					QueueItem(iterSockets->second.soap);
-					// Remove socket from listen list for now, since we're already handling data there and don't
-					// want to interfere with the thread that is now handling that socket. It will be passed back
-					// to us when the request is done.
-					m_setSockets.erase(iterSockets);
+					if (epevents[i].events & EPOLLHUP) {
+						kopano_end_soap_connection(iterSockets->second.soap);
+						soap_free(iterSockets->second.soap);
+						m_setSockets.erase(iterSockets);
+					} else {
+						QueueItem(iterSockets->second.soap);
+						// Remove socket from listen list for now, since we're already handling data there and don't
+						// want to interfere with the thread that is now handling that socket. It will be passed back
+						// to us when the request is done.
+						m_setSockets.erase(iterSockets);
+					}
 				}
 			}
 		}
