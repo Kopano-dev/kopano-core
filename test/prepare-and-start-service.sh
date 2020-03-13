2017d28a0a9ebf6e4cdfb8dbfc210cd36c2494ee
Simon Eisenmann <s.eisenmann@kopano.com>
2020-03-13 12:10:29 +0100

    test: ensure to create kscriptrun at the right location

--- test/prepare-and-start-service.sh
+++ test/prepare-and-start-service.sh
@@ -15,7 +15,10 @@ fi
 
 ln -svnf $WORKSPACE/.libs/kopano-* /usr/sbin/
 
-mkdir /usr/share/kopano
+mkdir -p /usr/local/libexec/kopano
+ln -svnf $WORKSPACE/.libs/kscriptrun /usr/local/libexec/kopano/
+
+mkdir -p /usr/share/kopano
 ln -svnf $WORKSPACE/installer/linux/ldap.openldap.cfg /usr/share/kopano
 ln -svnf $WORKSPACE/installer/linux/ldap.propmap.cfg /usr/share/kopano
 
