10a6b4eadf92dee0e32bbe50ff384b5eade887ae
Jan Engelhardt <jengelh@inai.de>
2020-04-29 21:21:29 +0200

    common: show errors from unix_popen_rw the right way
    The wrong error string may have been printed if there was an error during
    unix_popen_rw.
    
    Fixes: bp/8.4.0-438-g5f486782
    References: KS-44538

--- common/UnixUtil.cpp
+++ common/UnixUtil.cpp
@@ -470,7 +470,7 @@ bool unix_system(const char *lpszLogName, const std::vector<std::string> &cmd,
 	int pid = unix_popen_rw(argv.get(), &fdin, &fdout, nullptr, env);
 	ec_log_debug("Running command: %s", cmdtxt.c_str());
 	if (pid < 0) {
-		ec_log_debug("popen(%s) failed: %s", cmdtxt.c_str(), strerror(errno));
+		ec_log_debug("popen(%s) failed: %s", cmdtxt.c_str(), strerror(-pid));
 		return false;
 	}
 	close(fdin);
