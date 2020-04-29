57530e862a1e669c002c6f0036b1903f2f121c89
Jan Engelhardt <jengelh@inai.de>
2020-04-29 11:38:01 +0200

    libserver: add CR_CONNECTION_ERROR to the reconnect triggers
    When mysql has been stopped (or the restart otherwise takes "long"), what
    happens is:
    
      * ECDatabase::Query ▶ KDatabase::Query fails with
        KCERR_DATABASE_ERROR; mysql_errno is CR_SERVER_LOST or
        CR_SERVER_GONE (connection severed during idle or statement,
        respectively)
    
      * ECDatabase::Query calls Close, which sets m_bInitialized=false
        and mysql_errno=0. Calls Connect (fails), sets
        mysql_errno=CR_CONNECTION_ERROR, which in turn is not tied to any
        reconnection attempt.
    
    Fixes: SVN r22334 (Aug 23 14:29:44 2010)
    References: KS-45596, KS-45601

--- provider/libserver/ECDatabaseMySQL.cpp
+++ provider/libserver/ECDatabaseMySQL.cpp
@@ -441,6 +441,12 @@ exit:
 	return er;
 }
 
+static inline bool should_reconnect(int e)
+{
+	return e == CR_SERVER_LOST || e == CR_SERVER_GONE_ERROR ||
+	       e == CR_CONNECTION_ERROR;
+}
+
 /**
  * Perform an SQL query on MySQL
  *
@@ -457,8 +463,9 @@ ECRESULT ECDatabase::Query(const std::string &strQuery)
 {
 	ECRESULT er = erSuccess;
 	int err = KDatabase::Query(strQuery);
+	auto sqlerr = mysql_errno(&m_lpMySQL);
 
-	if(err && (mysql_errno(&m_lpMySQL) == CR_SERVER_LOST || mysql_errno(&m_lpMySQL) == CR_SERVER_GONE_ERROR)) {
+	if (err != 0 && should_reconnect(sqlerr)) {
 		ec_log_warn("SQL [%08lu] info: Try to reconnect", m_lpMySQL.thread_id);
 		er = Close();
 		if(er != erSuccess)
