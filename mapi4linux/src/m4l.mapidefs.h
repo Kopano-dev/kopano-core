3ab86d9e75d7636857863d90847d5a6292348f54
Jan Engelhardt <jengelh@inai.de>
2020-06-07 14:11:02 +0200

    mapi4linux: remove unused M4LMAPITable class
    M4L is not backed by any remote data, so it naturally uses
    ECMemTableView to provide the IMAPITable interface and does not need
    a M4LMAPITable class of its own.
    
    Fixes: SVN r544 (2005-06-03), r3031 (2006-02-10)
    References: KC-1789

--- mapi4linux/src/m4l.mapidefs.h
+++ mapi4linux/src/m4l.mapidefs.h
@@ -49,34 +49,6 @@ public:
 	virtual HRESULT QueryInterface(const IID &, void **) override;
 };
 
-class M4LMAPITable final : public KC::ECUnknown, public IMAPITable {
-public:
-	virtual HRESULT GetLastError(HRESULT result, unsigned int flags, MAPIERROR **) override;
-	virtual HRESULT Advise(unsigned int evt_mask, IMAPIAdviseSink *, unsigned int *conn) override;
-	virtual HRESULT Unadvise(unsigned int conn) override;
-	virtual HRESULT GetStatus(unsigned int *table_status, unsigned int *table_type) override;
-	virtual HRESULT SetColumns(const SPropTagArray *, unsigned int flags) override;
-	virtual HRESULT QueryColumns(unsigned int flags, SPropTagArray **) override;
-	virtual HRESULT GetRowCount(unsigned int flags, unsigned int *count) override;
-	virtual HRESULT SeekRow(BOOKMARK origin, int row_count, int *rows_sought) override;
-	virtual HRESULT SeekRowApprox(unsigned int num, unsigned int denom) override;
-	virtual HRESULT QueryPosition(unsigned int *row, unsigned int *num, unsigned int *denom) override;
-	virtual HRESULT FindRow(const SRestriction *, BOOKMARK origin, unsigned int flags) override;
-	virtual HRESULT Restrict(const SRestriction *, unsigned int flags) override;
-	virtual HRESULT CreateBookmark(BOOKMARK *pos) override;
-	virtual HRESULT FreeBookmark(BOOKMARK pos) override;
-	virtual HRESULT SortTable(const SSortOrderSet *sort_crit, unsigned int flags) override;
-	virtual HRESULT QuerySortOrder(SSortOrderSet **sort_crit) override;
-	virtual HRESULT QueryRows(int row_count, unsigned int flags, SRowSet **) override;
-	virtual HRESULT Abort() override;
-	virtual HRESULT ExpandRow(unsigned int ik_size, BYTE *inst_key, unsigned int row_count, unsigned int flags, SRowSet **, unsigned int *more_rows) override;
-	virtual HRESULT CollapseRow(unsigned int ik_size, BYTE *inst_key, unsigned int flags, unsigned int *row_count) override;
-	virtual HRESULT WaitForCompletion(unsigned int flags, unsigned int timeout, unsigned int *table_status) override;
-	virtual HRESULT GetCollapseState(unsigned int flags, unsigned int ik_size, BYTE *inst_key, unsigned int *coll_size, BYTE **collapse_state) override;
-	virtual HRESULT SetCollapseState(unsigned int flags, unsigned int coll_size, BYTE *collapse_state, BOOKMARK *loc) override;
-	virtual HRESULT QueryInterface(const IID &, void **) override;
-};
-
 class M4LProviderAdmin KC_FINAL_OPG : public KC::ECUnknown, public IProviderAdmin {
 private:
 	M4LMsgServiceAdmin* msa;
