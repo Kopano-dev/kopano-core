a530bee45b7fd4ecfd97da865bbaba14bb94e750
Jelle van der Waa <j.vanderwaa@kopano.com>
2020-07-03 16:29:27 +0200

    spooler: remove wildcard imports for examplerules
    Remove wildcard imports and clean up PEP8 style.

--- spooler/python/plugins/examplerules.py
+++ spooler/python/plugins/examplerules.py
@@ -1,10 +1,15 @@
 # SPDX-License-Identifier: AGPL-3.0-only
-import MAPI
-from MAPI.Util import *
-from MAPI.Time import *
-from MAPI.Struct import *
 
-from plugintemplates import *
+from MAPI import ROW_ADD, FL_IGNORECASE, FL_SUBSTRING
+from MAPI.Util import SPropValue
+from MAPI.Struct import actMoveCopy, ACTION, ACTIONS, ROWENTRY, SContentRestriction
+from MAPI.Tags import (PR_ENTRYID, PR_IPM_WASTEBASKET_ENTRYID, PR_RULE_LEVEL,
+                       PR_RULE_NAME, PR_RULE_PROVIDER, PR_RULE_STATE, PR_RULE_SEQUENCE,
+                       PR_RULE_CONDITION, PR_SUBJECT, ACTTYPE,
+                       PR_RULE_ACTIONS, ST_ENABLED, EDK_RULES_VERSION)
+
+from plugintemplates import IMapiDAgentPlugin, MP_CONTINUE
+
 
 class examplerules1(IMapiDAgentPlugin):
 
@@ -13,18 +18,15 @@ class examplerules1(IMapiDAgentPlugin):
         storeid = props[0].Value
         folderid = props[1].Value
 
-        rowlist = [ ROWENTRY(
-                        ROW_ADD,
-                            [   SPropValue(PR_RULE_LEVEL, 0),
-                                SPropValue(PR_RULE_NAME, "dagenttest"),
-                                SPropValue(PR_RULE_PROVIDER, "RuleOrganizer"),
-                                SPropValue(PR_RULE_STATE, ST_ENABLED),
-                                SPropValue(PR_RULE_SEQUENCE, 1),
-                                SPropValue(PR_RULE_ACTIONS, ACTIONS(EDK_RULES_VERSION, [ACTION(ACTTYPE.OP_MOVE, 0x00000000, None, None, 0x00000000, actMoveCopy(storeid, folderid) ) ] ) ),
-                                SPropValue(PR_RULE_CONDITION, SContentRestriction(FL_SUBSTRING|FL_IGNORECASE, PR_SUBJECT, SPropValue(PR_SUBJECT, 'rulestest') ) )
-                            ]
-                        )
-                ]
+        rowlist = [ROWENTRY(ROW_ADD,
+                            [SPropValue(PR_RULE_LEVEL, 0),
+                             SPropValue(PR_RULE_NAME, "dagenttest"),
+                             SPropValue(PR_RULE_PROVIDER, "RuleOrganizer"),
+                             SPropValue(PR_RULE_STATE, ST_ENABLED),
+                             SPropValue(PR_RULE_SEQUENCE, 1),
+                             SPropValue(PR_RULE_ACTIONS, ACTIONS(EDK_RULES_VERSION, [ACTION(ACTTYPE.OP_MOVE, 0x00000000, None, None, 0x00000000, actMoveCopy(storeid, folderid))])),
+                             SPropValue(PR_RULE_CONDITION, SContentRestriction(FL_SUBSTRING | FL_IGNORECASE, PR_SUBJECT, SPropValue(PR_SUBJECT, 'rulestest')))])
+                   ]
         rulestable.ModifyTable(0, rowlist)
 
         return MP_CONTINUE
