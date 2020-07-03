db8b4d8bbd1d3409afbe11702c52cec6a0e037b7
Jelle van der Waa <j.vanderwaa@kopano.com>
2020-07-03 15:44:40 +0200

    spooler: cleanup Python imports and PEP8 style
    Remove wildcard import from the disclaimer plugin and fixup PEP8
    styling.

--- spooler/python/plugins/disclaimer.py
+++ spooler/python/plugins/disclaimer.py
@@ -1,10 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-only
-import MAPI
-from MAPI.Util import *
-from MAPI.Time import *
-from MAPI.Struct import *
 
-from plugintemplates import *
+from MAPI import (WrapCompressedRTFStream, PT_ERROR, MAPI_MODIFY, MAPI_E_NOT_FOUND,
+                  MAPI_E_NOT_ENOUGH_MEMORY, STREAM_SEEK_END)
+from MAPI.Util import PROP_TYPE
+from MAPI.Tags import (PR_NULL, PR_BODY_W, PR_RTF_COMPRESSED, PR_HTML,
+                       PR_RTF_IN_SYNC, PR_USER_ENTRYID, PR_EC_COMPANY_NAME_W,
+                       PR_INTERNET_CPID,
+                       IID_IStream)
+
+from plugintemplates import MP_CONTINUE, IMapiSpoolerPlugin
 
 import os
 import codecs
@@ -16,101 +20,99 @@ class Disclaimer(IMapiSpoolerPlugin):
 
     def bestBody(self, message):
         tag = PR_NULL
-        bodytag = PR_BODY_W # todo use flags to support PR_BODY_A
+        bodytag = PR_BODY_W  # todo use flags to support PR_BODY_A
         props = message.GetProps([bodytag, PR_HTML, PR_RTF_COMPRESSED, PR_RTF_IN_SYNC], 0)
 
         if (props[3].ulPropTag != PR_RTF_IN_SYNC):
             return tag
 
-        if((props[0].ulPropTag == bodytag or ( PROP_TYPE(props[0].ulPropTag) == PT_ERROR and props[0].Value == MAPI_E_NOT_ENOUGH_MEMORY) ) and
+        if((props[0].ulPropTag == bodytag or (PROP_TYPE(props[0].ulPropTag) == PT_ERROR and props[0].Value == MAPI_E_NOT_ENOUGH_MEMORY)) and
            (PROP_TYPE(props[1].ulPropTag) == PT_ERROR and props[1].Value == MAPI_E_NOT_FOUND) and
            (PROP_TYPE(props[2].ulPropTag) == PT_ERROR and props[2].Value == MAPI_E_NOT_FOUND)):
             tag = bodytag
-        elif((props[1].ulPropTag == PR_HTML or ( PROP_TYPE(props[1].ulPropTag) == PT_ERROR and props[1].Value == MAPI_E_NOT_ENOUGH_MEMORY) ) and
+        elif((props[1].ulPropTag == PR_HTML or (PROP_TYPE(props[1].ulPropTag) == PT_ERROR and props[1].Value == MAPI_E_NOT_ENOUGH_MEMORY)) and
              (PROP_TYPE(props[0].ulPropTag) == PT_ERROR and props[0].Value == MAPI_E_NOT_ENOUGH_MEMORY) and
              (PROP_TYPE(props[2].ulPropTag) == PT_ERROR and props[2].Value == MAPI_E_NOT_ENOUGH_MEMORY) and
-             props[3].Value == False):
+             not props[3].Value):
             tag = PR_HTML
-        elif((props[2].ulPropTag == PR_RTF_COMPRESSED or ( PROP_TYPE(props[2].ulPropTag) == PT_ERROR and props[2].Value == MAPI_E_NOT_ENOUGH_MEMORY) ) and
+        elif((props[2].ulPropTag == PR_RTF_COMPRESSED or (PROP_TYPE(props[2].ulPropTag) == PT_ERROR and props[2].Value == MAPI_E_NOT_ENOUGH_MEMORY)) and
              (PROP_TYPE(props[0].ulPropTag) == PT_ERROR and props[0].Value == MAPI_E_NOT_ENOUGH_MEMORY) and
              (PROP_TYPE(props[1].ulPropTag) == PT_ERROR and props[1].Value == MAPI_E_NOT_FOUND) and
-             props[3].Value == True):
+             props[3].Value):
             tag = PR_RTF_COMPRESSED
 
         return tag
 
     def getCharSetByCP(self, codepage):
-        cp2char = { 20106:        "DIN_66003",
-                    20108:        "NS_4551-1",
-                    20107:     "SEN_850200_B",
-                    950:          "big5",
-                    50221:      "csISO2022JP",
-                    51932:           "euc-jp",
-                    51936:           "euc-cn",
-                    51949:           "euc-kr",
-                    949:           "euc-kr",
-                    949:            "cp949",
-                    949:   "ks_c_5601-1987",
-                    936:          "gb18030",
-                    936:           "gb2312",
-                    936:           "GBK",
-                    52936:         "csgb2312",
-                    852:           "ibm852",
-                    866:           "ibm866",
-                    50220:      "iso-2022-jp",
-                    50222:      "iso-2022-jp",
-                    50225:      "iso-2022-kr",
-                    1252:     "windows-1252",
-                    1252:       "iso-8859-1",
-                    28591:       "iso-8859-1",
-                    28592:       "iso-8859-2",
-                    28593:       "iso-8859-3",
-                    28594:       "iso-8859-4",
-                    28595:       "iso-8859-5",
-                    28596:       "iso-8859-6",
-                    28597:       "iso-8859-7",
-                    28598:       "iso-8859-8",
-                    28598:     "iso-8859-8-i",
-                    28599:       "iso-8859-9",
-                    28603:      "iso-8859-13",
-                    28605:      "iso-8859-15",
-                    20866:           "koi8-r",
-                    21866:           "koi8-u",
-                    932:        "shift-jis",
-                    1200:          "unicode",
-                    1201:       "unicodebig",
-                    65000:            "utf-7",
-                    65001:            "utf-8",
-                    1250:     "windows-1250",
-                    1251:     "windows-1251",
-                    1253:     "windows-1253",
-                    1254:     "windows-1254",
-                    1255:     "windows-1255",
-                    1256:     "windows-1256",
-                    1257:     "windows-1257",
-                    1258:     "windows-1258",
-                    874:      "windows-874",
-                    20127:         "us-ascii" }
-        try:
-            return cp2char[codepage]
-        except:
-            return "us-ascii"
+        cp2char = {20106:        "DIN_66003",
+                   20108:        "NS_4551-1",
+                   20107:     "SEN_850200_B",
+                   950:          "big5",
+                   50221:      "csISO2022JP",
+                   51932:           "euc-jp",
+                   51936:           "euc-cn",
+                   51949:           "euc-kr",
+                   949:           "euc-kr",
+                   949:            "cp949",
+                   949:   "ks_c_5601-1987",
+                   936:          "gb18030",
+                   936:           "gb2312",
+                   936:           "GBK",
+                   52936:         "csgb2312",
+                   852:           "ibm852",
+                   866:           "ibm866",
+                   50220:      "iso-2022-jp",
+                   50222:      "iso-2022-jp",
+                   50225:      "iso-2022-kr",
+                   1252:     "windows-1252",
+                   1252:       "iso-8859-1",
+                   28591:       "iso-8859-1",
+                   28592:       "iso-8859-2",
+                   28593:       "iso-8859-3",
+                   28594:       "iso-8859-4",
+                   28595:       "iso-8859-5",
+                   28596:       "iso-8859-6",
+                   28597:       "iso-8859-7",
+                   28598:       "iso-8859-8",
+                   28598:     "iso-8859-8-i",
+                   28599:       "iso-8859-9",
+                   28603:      "iso-8859-13",
+                   28605:      "iso-8859-15",
+                   20866:           "koi8-r",
+                   21866:           "koi8-u",
+                   932:        "shift-jis",
+                   1200:          "unicode",
+                   1201:       "unicodebig",
+                   65000:            "utf-7",
+                   65001:            "utf-8",
+                   1250:     "windows-1250",
+                   1251:     "windows-1251",
+                   1253:     "windows-1253",
+                   1254:     "windows-1254",
+                   1255:     "windows-1255",
+                   1256:     "windows-1256",
+                   1257:     "windows-1257",
+                   1258:     "windows-1258",
+                   874:      "windows-874",
+                   20127:         "us-ascii"}
+
+        return cp2char.get(codepage, "us-ascii")
 
     def getDisclaimer(self, extension, company):
-        if company == None:
+        if not company:
             company = 'default'
 
         name = os.path.join(self.disclaimerdir, company + '.' + extension)
 
-        self.logger.logDebug("*--- Open disclaimer file '%s'" % (name) )
+        self.logger.logDebug("*--- Open disclaimer file '%s'" % (name))
 
         return open(name, 'rb').read()
 
     def PreSending(self, session, addrbook, store, folder, message):
 
-        if os.path.isdir(self.disclaimerdir) == False:
-           self.logger.logWarn("!--- Disclaimer directory '%s' doesn't exist." % self.disclaimerdir)
-           return MP_CONTINUE,
+        if not os.path.isdir(self.disclaimerdir):
+            self.logger.logWarn("!--- Disclaimer directory '%s' doesn't exist." % self.disclaimerdir)
+            return MP_CONTINUE,
 
         company = None
 
@@ -121,8 +123,7 @@ class Disclaimer(IMapiSpoolerPlugin):
             userprops = currentuser.GetProps([PR_EC_COMPANY_NAME_W], 0)
             if userprops[0].ulPropTag == PR_EC_COMPANY_NAME_W and len(userprops[0].Value) > 0:
                 company = userprops[0].Value.encode("utf-8")
-                self.logger.logDebug("*--- Company name is '%s'" % (company) )
-
+                self.logger.logDebug("*--- Company name is '%s'" % (company))
 
         bodytag = self.bestBody(message)
 
