018f734220ca1a8f57746ba622152ad65bf19ffe
Simon Eisenmann <s.eisenmann@kopano.com>
2020-10-06 11:42:32 -0400

    pyko: add upstream reference and license for queue based logger
    Fixes: KC-1859
    (cherry picked from commit 1b30091c16f5758abad89b4ea8acf5fce19ba994)

--- swig/python/kopano/kopano/log.py
+++ swig/python/kopano/kopano/log.py
@@ -4,6 +4,25 @@ Part of the high-level python bindings for Kopano
 
 Copyright 2005 - 2016 Zarafa and its licensors (see LICENSE file)
 Copyright 2016 - 2019 Kopano and its licensors (see LICENSE file)
+
+Parts of this file are based on code from Vinay Sajip as found at
+https://gist.github.com/vsajip/820132
+
+Copyright 2011 - Vinay Sajip. All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee is hereby granted,
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in
+supporting documentation, and that the name of Vinay Sajip
+not be used in advertising or publicity pertaining to distribution
+of the software without specific, written prior permission.
+VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 """
 
 import contextlib
