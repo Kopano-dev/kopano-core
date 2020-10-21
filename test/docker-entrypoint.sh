f14390713685187048841bef3c8ff0570733c36d
Simon Eisenmann <simon@longsleep.org>
2020-10-21 09:26:18 +0200

    Dockerfile.build: backport
    This commit backports Docker based build support which is a requirement
    to run to be able to run the test suite in a future commit.
    
    Source revision: f91d7d7ca020f0b577cb694ead2873e5678e9214

---
+++ test/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+WORKSPACE=${WORKSPACE:-$(pwd)}
+
+export LD_LIBRARY_PATH=${WORKSPACE}/.libs:${WORKSPACE}/swig/python/.libs
+export PYTHONPATH=${WORKSPACE}/swig/python:${WORKSPACE}/swig/python/.libs:${WORKSPACE}/swig/python/kopano/
+export MAPI_CONFIG_PATH=${WORKSPACE}/provider/client
+
+if [ -f /kopano/start-service.sh ]; then
+	set -- /kopano/start-service.sh "$@"
+fi
+
+exec "$@"
