512457466b87039c6a8d25887fdaca6173619546
João Gouveia <jgouveia@kopano.com>
2021-03-23 01:48:46 +1400

    Set limit on header size to prevent bad alloc
    This sets a hard limit of 64 KiB to the header to prevent a memory
    allocation exception from being thrown during the parsing of the request
    headers.

--- caldav/Http.cpp
+++ caldav/Http.cpp
@@ -107,6 +107,8 @@ HRESULT Http::HrReadHeaders()
 	std::string strBuffer;
 	ULONG n = 0;
 	std::map<std::string, std::string>::iterator iHeader = mapHeaders.end();
+	static constexpr std::size_t MAX_HEADER_LENGTH = 65536;
+	std::size_t numOfBytesRead = 0;
 
 	ec_log_debug("Receiving headers:");
 	do
@@ -116,6 +118,11 @@ HRESULT Http::HrReadHeaders()
 			return hr;
 		if (strBuffer.empty())
 			break;
+
+		numOfBytesRead += strBuffer.size();
+		if(numOfBytesRead > MAX_HEADER_LENGTH) {
+			return MAPI_E_TOO_BIG;
+		}
 
 		if (n == 0) {
 			m_strAction = strBuffer;
