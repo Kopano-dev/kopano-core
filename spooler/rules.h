fc6f0950aa4fe4488e7ba897dfbc2d5e939d5088
João Gouveia <j.gouveia@kopano.com>
2021-04-13 21:44:02 +1400

    Add config headers to inhibit auto reply and forwd
    This creates two new configurations for users to add a list of headers
    that inhibit auto replying and auto forwarding e-mails.
    Caveat: Previous algorithm was using a hash map which provided better
    complexity. To simplify the algorithm this is now done with vectors
    however I do not expect it to have any noticable impact due to them
    have a relatively small size. We can always improve it if necessary.
    References: KC-1525
    
    (cherry picked from commit f06c66d2ef581b3fd94f6e86d943c00f6fc9982e)

--- spooler/rules.h
+++ spooler/rules.h
@@ -3,14 +3,16 @@
  * Copyright 2005 - 2016 Zarafa and its licensors
  */
 #pragma once
+
+#include "PyMapiPlugin.h"
+#include "StatsClient.h"
+
 #include <mapidefs.h>
 #include <mapix.h>
 #include <string>
 #include <vector>
-#include "PyMapiPlugin.h"
-#include "StatsClient.h"
 
 class PyMapiPlugin;
 
 extern HRESULT HrProcessRules(const std::string &recip, pym_plugin_intf *, IMAPISession *, IAddrBook *, IMsgStore *, IMAPIFolder *inbox, IMessage *, KC::StatsClient *);
-extern bool dagent_avoid_autoreply(const std::vector<std::string> &headers);
+extern bool dagent_header_present(const std::vector<std::string> &headers, const std::vector<std::string> &patterns);
