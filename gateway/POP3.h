Kopano-dev/kopano-core

commit a6228e429ab5b7f5ace928aa03e161fb7890bcfc

Limit memory allocation for unauthorized client data
Limiting the amount of data the server accepts when the client is not
authenticated helps mitigate DDoS attacks that cause the server to crash
due running out of memory. Valid, unauthenticated commands, do not
require much memory.

References: KC-1983
(cherry picked from commit 30c31841f3605c781376ec683e3035f245db8f13)

gateway/POP3.h
@@ -29,8 +29,10 @@ public:
 	POP3(const char *path, std::shared_ptr<KC::ECChannel>, std::shared_ptr<KC::ECConfig>);
 	~POP3();
 
+	bool isAuthenticated() const { return lpStore != nullptr; }
+
 	// getTimeoutMinutes: 5 min when logged in otherwise 1 min
-	int getTimeoutMinutes() const { return lpStore == nullptr ? 1 : 5; }
+	virtual int getTimeoutMinutes() const override { return isAuthenticated() ? 5 : 1; }
 	HRESULT HrSendGreeting(const std::string &strHostString);
 	HRESULT HrCloseConnection(const std::string &strQuitMsg);
 	HRESULT HrProcessCommand(const std::string &strInput);
@@ -64,7 +66,10 @@ private:
 	HRESULT HrMakeMailList();
 	HRESULT HrLogin(const std::string &strUsername, const std::string &strPassword);
 	std::string DotFilter(const char *input);
+<<<<<<< HEAD
 	BOOL IsAuthorized() const { return !!lpStore; }
+=======
+>>>>>>> 30c31841f... Limit memory allocation for unauthorized client data
 
 	KC::object_ptr<IMAPISession> lpSession;
 	KC::object_ptr<IMsgStore> lpStore;

common/ECChannel.cpp
@@ -478,21 +478,25 @@ HRESULT ECChannel::HrGets(char *szBuffer, size_t ulBufSize, size_t *lpulRead)
  * @return MAPI_ERROR_CODE
  * @retval MAPI_E_TOO_BIG more data in the network buffer than requested to read
  */
-HRESULT ECChannel::HrReadLine(std::string &strBuffer, size_t ulMaxBuffer)
+HRESULT ECChannel::HrReadLine(std::string &strBuffer, std::size_t ulMaxBuffer)
 {
-	size_t ulRead = 0;
-	char buffer[65536];
+	std::size_t ulRead = 0;
+	auto buffer = std::make_unique<char[]>(ulMaxBuffer);
 
 	// clear the buffer before appending
 	strBuffer.clear();
 	do {
-		auto hr = HrGets(buffer, 65536, &ulRead);
-		if (hr != hrSuccess)
+		auto hr = HrGets(buffer.get(), ulMaxBuffer, &ulRead);
+		if (hr != hrSuccess) {
 			return hr;
-		strBuffer.append(buffer, ulRead);
-		if (strBuffer.size() > ulMaxBuffer)
+		}
+
+		if (strBuffer.size() + ulRead > ulMaxBuffer) {
 			return MAPI_E_TOO_BIG;
-	} while (ulRead == 65535);	// zero-terminator is not counted
+		}
+
+		strBuffer.append(buffer.get(), ulRead);
+	} while (ulRead == ulMaxBuffer - 1); // NUL terminator is not counted
 	return hrSuccess;
 }
 

gateway/ClientProto.h
@@ -22,6 +22,7 @@ public:
 	{};
 	virtual ~ClientProto(void) = default;
 	virtual int getTimeoutMinutes() const = 0;
+	virtual bool isAuthenticated() const = 0;
 	virtual bool isContinue() const { return false; }; // imap only
 	virtual HRESULT HrSendGreeting(const std::string &strHostString) = 0;
 	virtual HRESULT HrCloseConnection(const std::string &strQuitMsg) = 0;

gateway/Gateway.cpp
@@ -67,6 +67,9 @@ struct socks {
 	std::vector<bool> pop3, ssl;
 };
 
+static constexpr std::size_t AUTHENTICATED_MAX_MESSAGE_SIZE = 65536;
+static constexpr std::size_t UNAUTHENTICATED_MAX_MESSAGE_SIZE = 256;
+
 static int daemonize = 1;
 static bool quit = 0, bThreads, g_dump_config;
 static std::atomic<bool> g_sighup_flag{false};
@@ -208,10 +211,13 @@ static void *Handler(void *lpArg)
 			bQuit = true;
 			break;
 		}
-
 		timeouts = 0;
-		inBuffer.clear();
-		hr = lpChannel->HrReadLine(inBuffer);
+
+		// If we're not logged in lets cap the maximum buffer size to
+		// prevent obvious DDOS attacks.
+		const std::size_t bufferSize = client->isAuthenticated() ?
+			AUTHENTICATED_MAX_MESSAGE_SIZE : UNAUTHENTICATED_MAX_MESSAGE_SIZE;
+		hr = lpChannel->HrReadLine(inBuffer, bufferSize);
 		if (hr != hrSuccess) {
 			if (errno)
 				ec_log_err("Failed to read line: %s", strerror(errno));
@@ -253,6 +259,10 @@ static void *Handler(void *lpArg)
 			ec_log_notice("gateway lost connection with storage server: remote side closed the connection.");
 			bQuit = true;
 		}
+		// Clear the buffer from memory.
+		// Note: .clear() does not actually release memory, since it
+		// only changes the size not the capacity
+		std::string().swap(inBuffer);
 	}
 exit:
 	ec_log_notice("Client %s thread exiting", lpChannel->peer_addr());

gateway/IMAP.cpp
@@ -76,6 +76,8 @@ static bool Prefix(const std::string &strInput, const std::string &strPrefix)
     return (strInput.compare(0, strPrefix.size(), strPrefix) == 0);
 }
 
+static constexpr std::size_t UNAUTHENTICATED_MAX_MESSAGE_SIZE = 256;
+
 IMAP::IMAP(const char *szServerPath, std::shared_ptr<ECChannel> ch,
     std::shared_ptr<ECConfig> cfg) :
 	ClientProto(szServerPath, std::move(ch), cfg)
@@ -239,7 +241,10 @@ HRESULT IMAP::HrProcessCommand(const std::string &strInput)
 	HRESULT hr = hrSuccess;
 	vector<string> strvResult;
 	std::string strTag;
-	ULONG ulMaxMessageSize = atoui(lpConfig->GetSetting("imap_max_messagesize"));
+	// If we're not logged in lets cap the maximum buffer size to prevent
+	// obvious DDOS attacks.
+	ULONG ulMaxMessageSize = isAuthenticated() ?
+		atoui(lpConfig->GetSetting("imap_max_messagesize")) : UNAUTHENTICATED_MAX_MESSAGE_SIZE;
 
 	static constexpr const struct {
 		const char *command;

gateway/IMAP.h
@@ -129,7 +129,8 @@ public:
 	~IMAP();
 
 	// getTimeoutMinutes: 30 min when logged in otherwise 1 min
-	int getTimeoutMinutes() const { return lpStore == nullptr ? 1 : 30; }
+	virtual int getTimeoutMinutes() const override { return isAuthenticated() ? 30 : 1; }
+	bool isAuthenticated() const { return lpStore != nullptr; }
 	bool isIdle() const { return m_bIdleMode; }
 	bool isContinue() const { return m_bContinue; }
 	HRESULT HrSendGreeting(const std::string &strHostString);

gateway/POP3.cpp
@@ -116,7 +116,7 @@ HRESULT POP3::HrProcessCommand(const std::string &strInput)
 		HrCmdQuit();
 		// let the gateway quit from the socket read loop
 		return MAPI_E_END_OF_SESSION;
-    } else if (!IsAuthorized()) {
+	} else if (!isAuthenticated()) {
 		HrResponse(POP3_RESP_ERR, "Invalid command");
 		ec_log_err("Not authorized for command \"%s\"", vWords[0].c_str());
 		return MAPI_E_CALL_FAILED;

commit f583daa3f3bc51f28b5a54ccdd30a0b41bc8aec1

Cleanup previous merge conflict
Fixes: a6228e429ab5b7f5ace928aa03e161fb7890bcfc

gateway/POP3.h
@@ -66,10 +66,6 @@ private:
 	HRESULT HrMakeMailList();
 	HRESULT HrLogin(const std::string &strUsername, const std::string &strPassword);
 	std::string DotFilter(const char *input);
-<<<<<<< HEAD
-	BOOL IsAuthorized() const { return !!lpStore; }
-=======
->>>>>>> 30c31841f... Limit memory allocation for unauthorized client data
 
 	KC::object_ptr<IMAPISession> lpSession;
 	KC::object_ptr<IMsgStore> lpStore;