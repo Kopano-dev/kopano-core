Kopano-dev/kopano-core

commit fdf476530f5a2a5e5541c20662ef51d98fd659cf

treewide: eliminate dead variable stores
clang-9 report goes like
StorageUtil.cpp:199:2: warning: Value stored to "ret" is never read
	ret = WriteProp(db, store_id, 0, &pv);

provider/libserver/ECStatsTables.cpp
@@ -587,15 +587,11 @@ ECRESULT ECUserStatsTable::QueryRowData(ECGenericObjectTable *lpThis,
 
 	gsoap_size_t i = 0;
 	for (const auto &row : *lpRowList) {
-		bool bNoObjectDetails = false, bNoQuotaDetails = false;
+		bool bNoObjectDetails = false;
 
 		if (lpUserManagement->GetObjectDetails(row.ulObjId, &objectDetails) != erSuccess)
 			// user gone missing since first list, all props should be set to ignore
-			bNoObjectDetails = bNoQuotaDetails = true;
-		else if (lpSession->GetSecurity()->GetUserQuota(row.ulObjId, false, &quotaDetails) != erSuccess)
-			// user gone missing since last call, all quota props should be set to ignore
-			bNoQuotaDetails = true;
-
+			bNoObjectDetails = true;
 		if (lpSession->GetSecurity()->GetUserSize(row.ulObjId, &llStoreSize) != erSuccess)
 			llStoreSize = 0;
 

ECtools/admin/admin.cpp
@@ -982,15 +982,12 @@ static HRESULT print_details(LPMAPISESSION lpSession,
 		if (hr != hrSuccess)
 			return kc_perror("Unable to resolve company administrator", hr);
 		hr = lpServiceAdmin->GetRemoteAdminList(cbObjectId, lpObjectId, 0, &cAdmins, &~lpECAdmins);
-		if (hr != hrSuccess) {
+		if (hr != hrSuccess)
 			kc_perror("Unable to display remote-admin list", hr);
-			hr = hrSuccess; /* Don't make error fatal */
-		}
 		hr = lpServiceAdmin->GetRemoteViewList(cbObjectId, lpObjectId, 0, &cViews, &~lpECViews);
-		if (hr != hrSuccess) {
+		if (hr != hrSuccess)
 			kc_perror("Unable to display remote-view list", hr);
-			hr = hrSuccess; /* Don't make error fatal */
-		}
+		hr = hrSuccess; /* Do not make errors fatal */
 		print_company_settings(lpECCompany, lpECUser);
 		show_store_provider(lpStore);
 		show_record_key("Store GUID:", lpStore);
@@ -1047,10 +1044,8 @@ static HRESULT print_details(LPMAPISESSION lpSession,
 			/* Ignore return value */
 		}
 		hr = lpServiceAdmin->GetGroupListOfUser(cbObjectId, lpObjectId, 0, &cGroups, &~lpECGroups);
-		if (hr != hrSuccess) {
+		if (hr != hrSuccess)
 			kc_perror("Unable to request groups for user", hr);
-			hr = hrSuccess; /* Don't make error fatal */
-		}
 		hr = ArchiveManage::Create(lpSession, NULL, converter.convert_to<LPTSTR>(lpszName), &ptrArchiveManage);
 		if (hr != hrSuccess) {
 			if (hr != MAPI_E_NOT_FOUND)
@@ -2753,10 +2748,8 @@ int main(int argc, char **argv)
 				kc_perror("Unable to open user store", hr);
 				goto exit;
 			}
-			hr = GetAutoAcceptSettings(lpUserStore, &bAutoAccept, &bDeclineConflict, &bDeclineRecurring, &auto_proc);
-			if (hr != hrSuccess)
-				// ignore and assume 'false' for all values
-				hr = hrSuccess;
+			/* Ignore and assume false for all values. */
+			GetAutoAcceptSettings(lpUserStore, &bAutoAccept, &bDeclineConflict, &bDeclineRecurring, &auto_proc);
 			if (mr_accept != -1)
 				bAutoAccept = mr_accept;
 			if (mr_decline_conflict != -1)

ECtools/archiver/ArchiveControlImpl.cpp
@@ -929,8 +929,7 @@ HRESULT ArchiveControlImpl::GetAllEntries(ArchiveHelperPtr ptrArchiveHelper, LPM
 	// that have not references to the primary store.
 	hr = ptrArchiveHelper->GetSpecialsRootFolder(&~ptrFolder);
 	if (hr == hrSuccess)
-		hr = AppendFolderEntries(ptrFolder, &setFolderExcludes);
-
+		AppendFolderEntries(ptrFolder, &setFolderExcludes);
 	ptrFolder.reset();
 
 	try {

ECtools/archiver/ArchiveManageImpl.cpp
@@ -240,9 +240,8 @@ HRESULT ArchiveManageImpl::AttachTo(LPMDB lpArchiveStore, const tstring &strFold
 
 	// Check if the archive is attached yet.
 	hr = ptrArchiveHelper->GetAttachedUser(&sAttachedUserEntryId);
-	if (hr == MAPI_E_NOT_FOUND)
-		hr = hrSuccess;
-	else if ( hr == hrSuccess && (!sAttachedUserEntryId.empty() && sAttachedUserEntryId != sUserEntryId)) {
+	if (hr == MAPI_E_NOT_FOUND) {
+	} else if (hr == hrSuccess && (!sAttachedUserEntryId.empty() && sAttachedUserEntryId != sUserEntryId)) {
 		tstring strUser;
 		tstring strFullname;
 

ECtools/archiver/helpers/ArchiveHelper.cpp
@@ -431,7 +431,6 @@ HRESULT ArchiveHelper::GetArchiveFolderFor(MAPIFolderPtr &ptrSourceFolder, Archi
 	if (hr == MAPI_E_CORRUPT_DATA) {
 		// If the list is corrupt, the folder will become unusable. We'll just create a new folder, which will most
 		// likely become the same folder (if the name hasn't changed).
-		hr = hrSuccess;
 	} else if (hr != hrSuccess)
 		return hr;
 

ECtools/archiver/main.cpp
@@ -460,6 +460,7 @@ int main(int argc, char **argv)
         } else {
 			filelogger->logf(EC_LOGLEVEL_DEBUG, "Archiver action: Detach archive \"%s\" on server \"%s\", folder \"%s\"", lpszArchive, lpszArchiveServer, lpszFolder);
             r = ptr->DetachFrom(lpszArchiveServer, toLPTST(lpszArchive, converter), toLPTST(lpszFolder, converter));
+			filelogger->logf(EC_LOGLEVEL_DEBUG, "Archiver result %d (%s)", r, ArchiveResultString(r));
         }
     }
     break;

ECtools/archiver/operations/copier.cpp
@@ -119,10 +119,8 @@ HRESULT Copier::Helper::ArchiveMessage(LPMESSAGE lpSource, const SObjectEntry *l
 	if (FAILED(hr))
 		return m_lpLogger->perr("Failed to copy message", hr);
 	hr = UpdateIIDs(lpSource, lpDest, &ptrPSAction);
-	if (hr != hrSuccess) {
+	if (hr != hrSuccess)
 		m_lpLogger->perr("Failed to update single instance IDs, continuing with copies.", hr);
-		hr = hrSuccess;
-	}
 
 	sPropArchFlags.ulPropTag = PROP_FLAGS;
 	sPropArchFlags.Value.ul = ARCH_NEVER_DELETE | ARCH_NEVER_STUB;
@@ -523,7 +521,6 @@ HRESULT Copier::DoProcessEntry(const SRow &proprow)
 			}
 			return hr;
 		}
-		hr = hrSuccess;
 		lstRollbacks.emplace_back(ptrRollback);
 		lstNewMsgArchives.emplace_back(ta->GetObjectEntry());
 	}

ECtools/idx_net.cpp
@@ -164,7 +164,6 @@ static HRESULT idx_startup_net(const char **argv)
 			auto ret = HrAccept(idx_poll[i].fd, &unique_tie(ch));
 			if (ret != hrSuccess) {
 				kc_perrorf("HrAccept failed", ret);
-				ret = hrSuccess;
 				continue;
 			}
 			pthread_t tid;

commit 1e8c9e95db9a62667388e2134ea4b94386c468c4

Quota props should be fetched from UserStatsTable
This seems to have been mistakenly broken in commit
fdf476530f5a2a5e5541c20662ef51d98fd659cf when attempting to clean-up
warnings. This commit was part of PR#2978

provider/libserver/ECStatsTables.cpp
@@ -572,12 +572,21 @@ ECRESULT ECUserStatsTable::QueryRowData(ECGenericObjectTable *lpThis,
 	gsoap_size_t i = 0;
 	for (const auto &row : *lpRowList) {
 		bool bNoObjectDetails = false;
+		bool bNoQuotaDetails = false;
 
-		if (lpUserManagement->GetObjectDetails(row.ulObjId, &objectDetails) != erSuccess)
+		if (lpUserManagement->GetObjectDetails(row.ulObjId, &objectDetails) != erSuccess) {
 			// user gone missing since first list, all props should be set to ignore
 			bNoObjectDetails = true;
-		if (lpSession->GetSecurity()->GetUserSize(row.ulObjId, &llStoreSize) != erSuccess)
+			bNoQuotaDetails = true;
+		}
+		else if (lpSession->GetSecurity()->GetUserQuota(row.ulObjId, false, &quotaDetails) != erSuccess) {
+			// user gone missing since last call, all quota props should be set to ignore
+			bNoQuotaDetails = true;
+		}
+
+		if (lpSession->GetSecurity()->GetUserSize(row.ulObjId, &llStoreSize) != erSuccess) {
 			llStoreSize = 0;
+		}
 
 		for (gsoap_size_t k = 0; k < lpsPropTagArray->__size; ++k) {
 			// default is error prop
@@ -598,32 +607,36 @@ ECRESULT ECUserStatsTable::QueryRowData(ECGenericObjectTable *lpThis,
 				break;
 
 			case PROP_ID(PR_EC_COMPANY_NAME):
-				if (bNoObjectDetails || lpUserManagement->GetObjectDetails(objectDetails.GetPropInt(OB_PROP_I_COMPANYID), &companyDetails) != erSuccess)
+				if (bNoObjectDetails || lpUserManagement->GetObjectDetails(objectDetails.GetPropInt(OB_PROP_I_COMPANYID), &companyDetails) != erSuccess) {
 					break;
+				}
 				// do we have a default copy function??
 				m.__union = SOAP_UNION_propValData_lpszA;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.lpszA = soap_strdup(soap, companyDetails.GetPropString(OB_PROP_S_FULLNAME).c_str());
 				break;
 			case PROP_ID(PR_EC_USERNAME):
-				if (bNoObjectDetails)
+				if (bNoObjectDetails) {
 					break;
+				}
 				// do we have a default copy function??
 				m.__union = SOAP_UNION_propValData_lpszA;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.lpszA = soap_strdup(soap, objectDetails.GetPropString(OB_PROP_S_LOGIN).c_str());
 				break;
 			case PROP_ID(PR_DISPLAY_NAME):
-				if (bNoObjectDetails)
+				if (bNoObjectDetails) {
 					break;
+				}
 				// do we have a default copy function??
 				m.__union = SOAP_UNION_propValData_lpszA;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.lpszA = soap_strdup(soap, objectDetails.GetPropString(OB_PROP_S_FULLNAME).c_str());
 				break;
 			case PROP_ID(PR_SMTP_ADDRESS):
-				if (bNoObjectDetails)
+				if (bNoObjectDetails) {
 					break;
+				}
 				// do we have a default copy function??
 				m.__union = SOAP_UNION_propValData_lpszA;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
@@ -643,31 +656,42 @@ ECRESULT ECUserStatsTable::QueryRowData(ECGenericObjectTable *lpThis,
 				break;
 			case PROP_ID(PR_EC_HOMESERVER_NAME):
 				// should always be this servername, see ::Load()
-				if (bNoObjectDetails || !lpSession->GetSessionManager()->IsDistributedSupported())
+				if (bNoObjectDetails || !lpSession->GetSessionManager()->IsDistributedSupported()) {
 					break;
+				}
 				// do we have a default copy function??
 				m.__union = SOAP_UNION_propValData_lpszA;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.lpszA = soap_strdup(soap, objectDetails.GetPropString(OB_PROP_S_SERVERNAME).c_str());
 				break;
 			case PROP_ID(PR_MESSAGE_SIZE_EXTENDED):
-				if (llStoreSize <= 0)
+				if (llStoreSize <= 0) {
 					break;
+				}
 				m.__union = SOAP_UNION_propValData_li;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.li = llStoreSize;
 				break;
 			case PROP_ID(PR_QUOTA_WARNING_THRESHOLD):
+				if (bNoQuotaDetails) {
+					break;
+				}
 				m.__union = SOAP_UNION_propValData_ul;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.ul = quotaDetails.llWarnSize / 1024;
 				break;
 			case PROP_ID(PR_QUOTA_SEND_THRESHOLD):
+				if (bNoQuotaDetails) {
+					break;
+				}
 				m.__union = SOAP_UNION_propValData_ul;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.ul = quotaDetails.llSoftSize / 1024;
 				break;
 			case PROP_ID(PR_QUOTA_RECEIVE_THRESHOLD):
+				if (bNoQuotaDetails) {
+					break;
+				}
 				m.__union = SOAP_UNION_propValData_ul;
 				m.ulPropTag = lpsPropTagArray->__ptr[k];
 				m.Value.ul = quotaDetails.llHardSize / 1024;